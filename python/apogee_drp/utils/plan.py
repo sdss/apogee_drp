64c4b984a9da294a9abecf6a9a097cbbddc0044c
David Nidever <dnidever@montana.edu>
2020-10-06 13:29:11 -0600

    Finished plan.load().

--- python/apogee_drp/utils/plan.py
+++ python/apogee_drp/utils/plan.py
@@ -74,4 +74,47 @@ def load(planfile,verbose=False,np=False,expand=False,plugmapm=False):
                     new[n][i] = apexp[i][n]
             plandata['APEXP'] = new
 
+
+    # Expand, add paths to the calibration IDs and add directories
+    if expand==True:
+        load = apload.ApLoad(apred=plandata['apred_vers'],telescope=plandata['telescope'])
+        calkeys = ['detid','bpmid','littrowid','persistid','persistmodelid','darkid',
+                   'flatid','psfid','fluxid','responseid','waveid','lsfid']
+        caltype = ['Detector','BPM','Littrow','Persist','PersistModel','Dark',
+                   'Flat','PSF','Flux','Response','Wave','LSF']
+        for i in range(len(calkeys)):
+            val = plandata.get(calkeys[i])
+            if val is not None:
+                calfile = os.path.dirname(load.filename(caltype[i],num=val,chips='a'))+'/'+str(val)
+                plandata[calkeys[i]] = calfile
+
+        # Add directores
+        spectro_dir = os.environ['APOGEE_REDUX']+'/'+plandata['apred_vers']+'/'
+        datadir = {'apo25m':os.environ['APOGEE_DATA_N'],'apo1m':os.environ['APOGEE_DATA_N'],
+                   'lco25m':os.environ['APOGEE_DATA_S']}[plandata['telescope']]+'/'
+        if plandata.get('plate_dir') is None:
+            plandata['plate_dir'] = spectro_dir+'visit/'+plandata['telescope']+'/'+\
+                                    str(plandata['plateid'])+'/'+str(plandata['mjd'])+'/'
+        if plandata.get('star_dir') is None:
+            plandata['star_dir'] = spectro_dir+'fields/'+plandata['telescope']
+
+        # Expand plugmap
+        val = plandata['plugmap']
+        if val is not None:
+            # Check that it has no path information
+            if val.find('/') == -1 and val.strip() != '':
+                # Add directory
+                plugfile = datadir+str(plandata['mjd'])+'/'
+                # Add plPlugMap prefix if necessary
+                if val.startswith('pl')==False:
+                    if plugmapm==False:
+                        plugfile += 'plPlugMapA-'
+                    else:
+                        plugfile += 'plPlugMapM-'
+                plugfile += val
+                # Add .par ending if necessary
+                if val.endswith('.yaml')==False:
+                    plugfile += '.yaml'
+                plandata['plugmap'] = plugfile
+
     return plandata
