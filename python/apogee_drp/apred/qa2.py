01127e6ad57fd78162bee84a11f6816373082b1e
drewchojnowski <drewski@nmsu.edu>
2020-10-23 14:30:55 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -334,7 +334,7 @@ def makePlateSum(load=None, telescope=None, ims=None, plate=None, mjd=None, fiel
         if ims[0] == 0: cframe = load.apPlate(int(plate), mjd)
         if ims[0] != 0:
             cframefile = load.filename('Cframe', plate=int(plate), mjd=mjd, num=ims[i], chips='c')
-            #cframefile = cframefile.replace('apCframe-','apCframe-c-')
+            cframefile = cframefile.replace('apCframe-','apCframe-c-')
             if os.path.exists(cframefile): cframe = load.apCframe(field, int(plate), mjd, ims[i])
 
         cframehdr = cframe['a'][0].header
