584f13af6a348fe01892b89b7faa3dd6ca168a97
drewchojnowski <drewski@nmsu.edu>
2020-10-25 14:11:32 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -1078,10 +1078,10 @@ def makePlotsHtml(load=None, telescope=None, ims=None, plate=None, mjd=None, fie
 
                 # PLOT 2c: S/N as calculated from ap1D frame
                 x = plSum2['HMAG'][science];    y = plSum2['SN'][science,i,1]
-                ax3.semilogy(x, y, marker='o', ms=10, mec='k', alpha=alpha, mfc='r', linestyle=None)
+                ax3.semilogy(x, y, marker='o', ms=10, mec='k', alpha=alpha, mfc='r', linestyle=' ')
                 if ntelluric > 0: 
                     x = plSum2['HMAG'][telluric];   y = plSum2['SN'][telluric,i,1]
-                    ax3.semilogy(x, y, marker='^', ms=10, mec='k', alpha=alpha, mfc='cyan', linestyle=None)
+                    ax3.semilogy(x, y, marker='^', ms=10, mec='k', alpha=alpha, mfc='cyan', linestyle=' ')
 
                 fig.subplots_adjust(left=0.14,right=0.98,bottom=0.05,top=0.99,hspace=0.1,wspace=0.0)
                 plt.savefig(plotfilefull)
