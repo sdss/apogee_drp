fd42eaeb2ae558b2eacec92cd7d102c45bd40484
drewchojnowski <drewski@nmsu.edu>
2020-10-25 13:33:22 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -1053,10 +1053,10 @@ def makePlotsHtml(load=None, telescope=None, ims=None, plate=None, mjd=None, fie
                 ax4.axes.xaxis.set_ticklabels([])
 
                 ax5.set_xlabel(r'$H$')
-                ax1.text(-0.08,0.50,r'mag = -2.5 * log$_{\rm 10}$(counts)',transform=ax1.transAxes,rotation=90,ha='left',va='center')
-                ax2.text(-0.08,0.50,r'$H$ - (mag+zero)',transform=ax2.transAxes,rotation=90,ha='left',va='center')
-                ax3.text(-0.08,0.50,r'S/N',transform=ax3.transAxes,rotation=90,ha='left',va='center')
-                ax4.text(-0.08,0.50,r'S/N',transform=ax4.transAxes,rotation=90,ha='left',va='center')
+                ax1.text(-0.15,0.50,r'mag = -2.5 * log$_{\rm 10}$(counts)',transform=ax1.transAxes,rotation=90,ha='left',va='center')
+                ax2.text(-0.15,0.50,r'$H$ - (mag+zero)',transform=ax2.transAxes,rotation=90,ha='left',va='center')
+                ax3.text(-0.15,0.50,r'S/N',transform=ax3.transAxes,rotation=90,ha='left',va='center')
+                ax4.text(-0.15,0.50,r'S/N',transform=ax4.transAxes,rotation=90,ha='left',va='center')
 
                 alpha = 0.6
 
