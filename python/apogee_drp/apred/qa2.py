87d472da5971f61ccec08506372c23c7ba239218
drewchojnowski <drewski@nmsu.edu>
2020-10-25 13:53:17 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -1064,7 +1064,7 @@ def makePlotsHtml(load=None, telescope=None, ims=None, plate=None, mjd=None, fie
                 x = plSum2['HMAG'];    y = plSum2['obsmag'][:,i,1]-plSum1['ZERO'][i]
                 ax1.scatter(x, y, marker='o', s=50, edgecolors='k', alpha=alpha, c='r')
                 if ntelluric > 0: 
-                    x = plSum2['HMAG'][telluric];   y = plSum2['obsmag'][telluric,i,1]
+                    x = plSum2['HMAG'][telluric];   y = plSum2['obsmag'][telluric,i,1]-plSum1['ZERO'][i]
                     ax1.scatter(x, y, marker='^', s=60, edgecolors='k', alpha=alpha, c='b')
 
                 fig.subplots_adjust(left=0.14,right=0.98,bottom=0.05,top=0.99,hspace=0.1,wspace=0.0)
