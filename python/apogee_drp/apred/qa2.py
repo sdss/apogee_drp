91e4f153311235dc37c3108f9896bdbc4e4235fb
drewchojnowski <drewski@nmsu.edu>
2020-10-25 14:38:10 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -1079,14 +1079,16 @@ def makePlotsHtml(load=None, telescope=None, ims=None, plate=None, mjd=None, fie
                 ax2.scatter(x, y, marker='^', s=60, edgecolors='k', alpha=alpha, c='cyan')
 
                 # PLOT 2c: S/N as calculated from ap1D frame
+                ax3.set_ylim(1,1000)
                 x = plSum2['HMAG'][science];    y = plSum2['SN'][science,i,1]
                 ax3.scatter(x, y, marker='^', s=60, edgecolors='k', alpha=alpha, c='r')
+                ax3.set_yscale('log')
 #                ax3.semilogy(x, y, marker='o', ms=10, mec='k', alpha=alpha, mfc='r', linestyle=' ')
                 x = plSum2['HMAG'][telluric];   y = plSum2['SN'][telluric,i,1]
                 ax3.scatter(x, y, marker='^', s=60, edgecolors='k', alpha=alpha, c='cyan')
-#                ax3.semilogy(x, y, marker='^', ms=10, mec='k', alpha=alpha, mfc='cyan', linestyle=' ')
                 ax3.set_yscale('log')
-                ax3.set_ylim(1,1000)
+#                ax3.semilogy(x, y, marker='^', ms=10, mec='k', alpha=alpha, mfc='cyan', linestyle=' ')
+
 
                 fig.subplots_adjust(left=0.16,right=0.98,bottom=0.05,top=0.99,hspace=0.1,wspace=0.0)
                 plt.savefig(plotfilefull)
