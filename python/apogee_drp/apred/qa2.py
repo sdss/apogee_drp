0e2b5b4e1459833a6f1b76cdd6bf37417167c2f7
drewchojnowski <drewski@nmsu.edu>
2020-10-26 12:48:45 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -688,6 +688,9 @@ def masterQApage(load=None, plate=None, mjd=None, field=None, fluxid=None, teles
             html.write('<TD> <A HREF='+'../plots/'+fluxfile+'.png><IMG SRC=../plots/'+fluxfile+'.png WIDTH=400></\n')
         tmp = load.filename('Flux', num=fluxid, chips=True).replace('apFlux-','apFlux-'+chips[0]+'-')
         blockfile = os.path.basename(tmp).replace('.fits','').replace('-a-','-block-')
+        html.write('</TABLE>\n')
+        html.write('<P> Fiber Blocks\n')
+        html.write('<TABLE BORDER=2><TR>\n')
         html.write('<TD> <A HREF='+'../plots/'+blockfile+'.png><IMG SRC=../plots/'+blockfile+'.png WIDTH=400></A>\n')
         html.write('</TABLE>\n')
 
