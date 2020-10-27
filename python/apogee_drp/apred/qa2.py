3a2ab5cd58025b2f0301c3f5197f0c322c5fc777
drewchojnowski <drewski@nmsu.edu>
2020-10-27 14:05:49 -0600

    updating qa2.py

--- python/apogee_drp/apred/qa2.py
+++ python/apogee_drp/apred/qa2.py
@@ -978,7 +978,9 @@ def makePlotsHtml(load=None, telescope=None, ims=None, plate=None, mjd=None, fie
                         ax1.set_xlabel(r'Wavelength [$\rm \AA$]')
                         ax1.set_ylabel(r'Flux')
 
-                        ax1.plot([WaveB,WaveG,WaveR], [FluxB,FluxG,FluxR], color='k', linewidth=1)
+                        ax1.plot(WaveB[np.argsort(WaveB)], FluxB[np.argsort(WaveB)], color='k', linewidth=1)
+                        ax1.plot(WaveG[np.argsort(WaveG)], FluxG[np.argsort(WaveG)], color='k', linewidth=1)
+                        ax1.plot(WaveR[np.argsort(WaveR)], FluxR[np.argsort(WaveR)], color='k', linewidth=1)
 
                         fig.subplots_adjust(left=0.06,right=0.995,bottom=0.16,top=0.97,hspace=0.2,wspace=0.0)
                         plt.savefig(plotfilefull)
