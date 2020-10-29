361ee5234d9a4c3f1b5af7515dd1bee6a373a2a8
drewchojnowski <drewski@nmsu.edu>
2020-10-29 14:41:00 -0600

    adding qa3.py

--- python/apogee_drp/apred/qa3.py
+++ python/apogee_drp/apred/qa3.py
@@ -1053,7 +1053,7 @@ def masterQAplots(load=None, ims=None, plate=None, mjd=None, instrument=None, ap
         ymaxtel = np.nanmax(y)
         ax3.semilogy(x, y, marker='o', ms=9, mec='k', alpha=alpha, mfc='dodgerblue', linestyle='')
         ymax = np.max([ymaxsci,ymaxtel])
-        ax2.set_ylim(0.01,ymax*1.05)
+        ax3.set_ylim(0.01,ymax*1.05)
 
         # overplot the target S/N line
         sntarget = 100 * np.sqrt(plSum1['EXPTIME'][i] / (3.0 * 3600))
