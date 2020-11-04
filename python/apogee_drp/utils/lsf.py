2c803c4f3f641d96e6d892918d20ed6d726c9715
David Nidever <dnidever@lsst.org>
2020-11-04 00:22:55 -0700

    Updates to lsf.py for fitting 2D LSF arrays.

--- python/apogee_drp/utils/lsf.py
+++ python/apogee_drp/utils/lsf.py
@@ -3,13 +3,23 @@
 import numpy as np
 import doppler
 from doppler import lsf as doplsf
-from .norm import apStarWave
+from scipy.optimize import curve_fit
+#from .norm import apStarWave
 
+logw0=4.179
+dlogw=6.e-6
+nw_apStar=8575
+def apStarWave() :
+    """ Returns apStar wavelengths
+    """
+    return 10.**(logw0+np.arange(nw_apStar)*dlogw)
+    
 
-class lsfghfitter:
-    def __init__(self,lsfpars):
+class LsfGHFitter:
+    def __init__(self,wave,lsfpars):
+        # Make sure lsfpars are at least 2D
         self.lsfpars = lsfpars
-        self.lsf = doplsf.GaussHermiteLsf(lsfpars=lsfpars,lsftype='Gauss-Hermite',lsfxtype='Pixels')
+        self.lsf = doplsf.GaussHermiteLsf(wave=wave,pars=lsfpars,lsftype='Gauss-Hermite',xtype='Pixels')
 
         # Useful information for parsing the array of coefficients
         params = doplsf.unpack_ghlsf_params(lsfpars)
@@ -21,32 +31,47 @@ class lsfghfitter:
         # GHpar = lsfarr[out['Horder']+4:out['Horder']+4+nGHcoefs] #all coeffs
         # wingarr = lsfarr[3+out['Horder']+1+nGHcoefs:]
         # Wpar = wingarr[out['nWpar']+2:]
-        self.wcoef0 = params['Horder']+4+nGHcoefs + 3+param['Horder']+1+nGHcoefs
+        self.wcoef0 = 3+params['Horder']+1+nGHcoefs + params['nWpar']+2
         self.wcoef1 = len(lsfpars)-1
-        self.nwcoefs = self.wcoef1-self.wcoef0+1        
-        
-    def model(self,x,*args):
-        """ Create the LSF model."""
+        self.nwcoefs = self.wcoef1-self.wcoef0+1
+        # Get the initial coefficients
+        ghcoefs = lsfpars[self.ghcoef0:self.ghcoef1+1]
+        wcoefs = lsfpars[self.wcoef0:self.wcoef1+1]
+        coeffs = np.hstack((ghcoefs,wcoefs))
+        self.coeffs = coeffs
+
+    def loadcoefs(self,coefs):
+        """ Load coefficients into the lsf parameters array."""
+
         lsfpars = self.lsf.pars.copy()
-        coef = np.array(args)
-        
         # Stuff in GH coefficients
-        ghcoefs = coef[0:self.nghcoefs]
-        lsfpars[self.ghcoef0:self.ghcoef1+1] = ghcoefs
+        ghcoefs = coefs[0:self.nghcoefs]
+        lsfpars[self.ghcoef0:self.ghcoef1+1,0] = ghcoefs
         # Stuff in wing coefficients
-        wcoefs = coef[self.nghcoefs:]
-        lsfpars[self.wcoef0:self.wcoef1+1] = wcoefs
-        self.lsf.pars = lsfpars
-        # Generate the LSF model
-        lsfmodel = self.lsf.anyarray(x,original=False)
+        wcoefs = coefs[self.nghcoefs:]
+        lsfpars[self.wcoef0:self.wcoef1+1,0] = wcoefs
+        return lsfpars
         
-        return lsf
+    def model(self,x,*args,flatten=True):
+        """ Create the LSF model."""
+        coef = np.array(args)
+        lsfpars = self.loadcoefs(coef)
+        self.lsf.pars = lsfpars
+        
+        # Generate the LSF model
+        lsfmodel = self.lsf.anyarray(x,xtype='pixel',order=0,nlsf=15,original=True)
+
+        if flatten==True:
+            return lsfmodel.flatten()   # must be 1D for 
+        else:
+            return lsfmodel
         
 
 def fitghpars(w,comblsf,initparams):
     """ Get LSF GH coefficients for a 2D LSF array."""
-    
-    fitter = lsfghfitter(initparams)
+
+    # Initialize the fitter
+    fitter = LsfGHFitter(w,initparams)
 
     # Downweight pixels where there is no LSF
     lsfsum = np.sum(comblsf,axis=1)
@@ -54,11 +79,22 @@ def fitghpars(w,comblsf,initparams):
     nbd = len(bd)
     sigma = comblsf*0+1.0
     sigma[bd] = 1e10
-
+    
     # bounds=bounds
-    pars, cov = curve_fit(fitter.model, w, comblsf, sigma=sigma, p0=pinit)
+    # Data must be flattened to 1D for curve_fit
+    pinit = fitter.coeffs.copy()
+    npix,nlsf = comblsf.shape
+    #w2 = np.tile(w,(nlsf,1)).T
+    #pars, cov = curve_fit(fitter.model, w2.flatten(), comblsf.flatten(), sigma=sigma.flatten(), p0=pinit)
+    x = np.arange(npix)
+    pars, cov = curve_fit(fitter.model, x, comblsf.flatten(), sigma=sigma.flatten(), p0=pinit)
     perr = np.sqrt(np.diag(cov))
-    return coef, perr
+
+    # Final LSF model and full parameter array
+    lsfmodel = fitter.model(x,*pars,flatten=False)
+    lsfpars = fitter.loadcoefs(pars)
+
+    return pars, perr, lsfpars, lsfmodel
 
 
 def lsfvisitcomb(visitfiles):
@@ -72,7 +108,7 @@ def lsfvisitcomb(visitfiles):
     specarr = []
     for vf in visitfiles:
         specarr.append(doppler.read(vf))
-
+        
     # Create LSF array
     wstar = apStarWave() 
     npix = len(wstar)
@@ -80,12 +116,16 @@ def lsfvisitcomb(visitfiles):
     lsfarr = np.zeros((npix,nlsf,nspec),np.float64)
     # Loop over spectra
     for s in range(nspec):
+        print(str(s+1)+' '+visitfiles[s])
         # Loop over orders
-        lsf1 = np.zeros((npix,lsf),np.float64)
+        lsf1 = np.zeros((npix,nlsf),np.float64)
         for o in range(3):
+            # Get the wstar pixels covered by this chip/order
             g, = np.where((wstar >= specarr[s].wave[:,o].min()) & (wstar <= specarr[s].wave[:,o].max()))
-            lsf1[g,:] += specarr[o].lsf.anyarray(wstar[g],xtype='wave',order=o,nlsf=15,original=False)
-
+            print('  '+str(o)+' '+str(len(g)))
+            lsf1[g,:] += specarr[s].lsf.anyarray(wstar[g],xtype='wave',order=o,nlsf=15,original=False)
+        lsfarr[:,:,s] = lsf1
+            
     # Get S/N per star
     snarr = np.zeros(nspec,float)
     for i in range(nspec):
@@ -98,20 +138,22 @@ def lsfvisitcomb(visitfiles):
         comblsf[:,:] += snarr[i]*lsfarr[:,:,i]
     totsnr = np.sum(snarr)
     comblsf /= totsnr  # normalize
-
+    
     # Make sure each pixel is normalized
     comblsf[comblsf<0.] = 0.
-    comblsf /= np.tile(np.sum(comblsf,axis=1),(nlsf,1)).T
-
+    totcomblsf = np.sum(comblsf,axis=1)
+    totcomblsf[totcomblsf<0.01] = 1.0           # deal with "missing" pixels
+    comblsf /= np.tile(totcomblsf,(nlsf,1)).T
+    
     # Initialize GH parameters
     #params = doplsf.unpack_ghlsf_params(specarr[0].lsf.pars[:,1])
     #params['Xoffset'] = npix//2
     #coeffs = np.hstack((params['GHcoefs'].flatten(),params['Wcoefs'].flatten()))
     lsfpars = specarr[0].lsf.pars[:,1]
-    lspars[1] = npix//2
+    lsfpars[1] = npix//2
     
     # Now fit LSF Gauss-Hermite parameters to the combined LSF
-    pars,cov = fitghpars(wstar,comblsf,lsfpars)
-
+    coefs,coeferr,finalpars,lsfmodel = fitghpars(wstar,comblsf,lsfpars)
     
+    return comblsf,coefs,coeferr,finalpars,lsfmodel
 
