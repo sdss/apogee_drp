694e9b3171951b5887f6b7c6a99d1fd40ffaf5ea
drewchojnowski <drewski@nmsu.edu>
2020-11-05 15:57:03 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1725,7 +1725,7 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None,fie
                 plotfile = 'aitoff_'+ptype+'.png'
                 print("----> makeMasterQApages: Making "+plotfile)
 
-                fig=plt.figure(figsize=(16,8))
+                fig=plt.figure(figsize=(13,8))
                 ax1 = fig.add_subplot(111, projection = 'aitoff')
                 ax1.grid(True)
                 #ax2 = fig.add_subplot(122, projection = 'aitoff')
