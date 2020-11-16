55109ba1de6d7748bd7ec56dc044be165bbe9dcc
drewchojnowski <drewski@nmsu.edu>
2020-11-16 13:59:04 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -176,7 +176,8 @@ def apqa(plate='15000', mjd='59146', telescope='apo25m', apred='daily', makeplat
                              starmag=None,flat=None, fixfiberid=fixfiberid, badfiberid=badfiberid,
                              clobber=clobber)
 
-            tmpims = np.array([0,ims[0]])
+            gd, = np.where(imsReduced == 1)
+            tmpims = np.array([0,ims[gd][0]])
             q = makePlateSum(load=load, plate=plate, mjd=mjd, telescope=telescope, field=field,
                              instrument=instrument, ims=tmpims, imsReduced=imsReduced,
                              plugmap=plugmap, survey=survey, mapper_data=mapper_data, 
