7e76ec4b8f74119337b161edc954783bd1fbea0a
drewchojnowski <drewski@nmsu.edu>
2020-11-16 14:41:09 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2312,7 +2312,8 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
         axmajlen = 7
         axminlen = 3.5
         msz = 100
-        nplots = 2
+        lonlabs = ['210','240','270','300','330','0','30','60','90','120','150']
+        nlon = len(lonlabs);  lonstart = 0.085;  lonsep = 0.083
 
         for j in range(nplots):
             if j == 0: ptype = 'galactic'
@@ -2324,10 +2325,10 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
             ax1 = fig.add_subplot(111, projection = 'aitoff')
             ax1.grid(True)
             ax1.axes.xaxis.set_ticklabels([])
-            lonlabs = ['210','240','270','300','330','0','30','60','90','120','150']
-            nlon = len(lonlabs);  lonstart = 0.085;  lonsep = 0.083
-            for t in range(nlon): 
-                ax1.text(lonstart+lonsep*t, 0.5, lonlabs[t], ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
+
+            ax1.text(0.5, 0.1, r'b = 0$^{\circ}$', ha='center', transform=ax1.transAxes)
+            #for t in range(nlon): 
+            #    ax1.text(lonstart+lonsep*t, 0.5, lonlabs[t], ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
             #ax1.text(0.165, 0.5, '240', ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
             #ax1.set_xticks([-150,-120,-90,-60,-30,0,30,60,90,120,150])
             #ax1.set_xticklabels(['210','240','270','300','330','0','30','60','90','120','150'])
