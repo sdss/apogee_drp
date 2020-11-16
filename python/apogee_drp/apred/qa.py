a38c4e725a50bac595d65aae4a76c529e91ffd49
drewchojnowski <drewski@nmsu.edu>
2020-11-16 14:36:51 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2325,10 +2325,10 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
             ax1.grid(True)
             ax1.axes.xaxis.set_ticklabels([])
             lonlabs = ['210','240','270','300','330','0','30','60','90','120','150']
-            nlon = len(lonlabs);  lonstart = 0.085;  lonsep = 0.08
+            nlon = len(lonlabs);  lonstart = 0.085;  lonsep = 0.085
             for t in range(nlon): 
                 ax1.text(lonstart+lonsep*t, 0.5, lonlabs[t], ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
-            ax1.text(0.165, 0.5, '240', ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
+            #ax1.text(0.165, 0.5, '240', ha='center', va='center', fontsize=fsz, transform=ax1.transAxes)
             #ax1.set_xticks([-150,-120,-90,-60,-30,0,30,60,90,120,150])
             #ax1.set_xticklabels(['210','240','270','300','330','0','30','60','90','120','150'])
             #ax2 = fig.add_subplot(122, projection = 'aitoff')
