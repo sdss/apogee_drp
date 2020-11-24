61f36f2efec99fe8d425f4ed2dfb99a6aa475679
drewchojnowski <drewski@nmsu.edu>
2020-11-24 12:46:48 -0700

    adding domeFlatPeaks.py

--- python/apogee_drp/apred/domeFlatPeaks.py
+++ python/apogee_drp/apred/domeFlatPeaks.py
@@ -64,7 +64,7 @@ def PlotFlats(apred='daily', telescope='apo25m',sep=50):
     plt.scatter(peaks,tot[peaks], marker='x', color='r')
 
     plt.plot(stot+200, color='k')
-    plt.scatter(speaks+200, stot[speaks], marker='x', color='r')
+    plt.scatter(speaks+200, stot[speaks], marker='x', color='g')
 
 
 #    for i in range(nplans):
