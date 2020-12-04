f5d99260fc3df884c5d781ace59b055d4e587264
drewchojnowski <drewski@nmsu.edu>
2020-12-04 13:07:21 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1037,9 +1037,9 @@ def makeObsQAplots(load=None, ims=None, imsReduced=None, plate=None, mjd=None, i
             if i == 1:
                 scicol = block[science]
                 telcol = block[telluric]
-            psci = ax.scatter(x, y, marker='*', s=210, edgecolors=scicol, alpha=0.8, c=scicol, cmap='tab10', vmin=0, vmax=10, label='Science')
+            psci = ax.scatter(x, y, marker='*', s=210, edgecolors='white', alpha=0.8, c=scicol, cmap='tab10', vmin=0, vmax=10, label='Science')
             x = hmagarr[telluric];  y = Vsum['SNR'][telluric]
-            ptel = ax.scatter(x, y, marker='o', s=100, edgecolors=telcol, alpha=0.8, c=telcol, cmap='tab10', vmin=0, vmax=10, label='Telluric')
+            ptel = ax.scatter(x, y, marker='o', s=100, edgecolors='white', alpha=0.8, c=telcol, cmap='tab10', vmin=0, vmax=10, label='Telluric')
 
             if i == 1:
                 ax_divider = make_axes_locatable(ax)
