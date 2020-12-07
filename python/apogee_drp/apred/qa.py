82a957397c71d6ebb4ce2577d199f67b882df1ab
drewchojnowski <drewski@nmsu.edu>
2020-12-07 11:08:05 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -58,7 +58,7 @@ sort_table_link = 'https://www.kryogenix.org/code/browser/sorttable/sorttable.js
 '''APQAMJD: Wrapper for running apqa for all plates on an mjd '''
 def apqaMJD(mjd='59146', observatory='apo', apred='daily', makeplatesum=True,
             makeplots=True, makespecplots=True, makemasterqa=True, makenightqa=True,
-            clobber=False):
+            clobber=True):
 
     # Find the list of plan files
     apodir = os.environ.get('APOGEE_REDUX')+'/'
