d5cd3a906a155a65fdd44a9f4882f0854b656a07
drewchojnowski <drewski@nmsu.edu>
2020-12-07 13:03:34 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2296,44 +2296,21 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
                     html.write('<TD>\n')
 
                 # Column 8: Mean moon phase
-                plates = np.array(glob.glob(apodir+apred+'/visit/'+telescope+'/*/*/'+cmjd+'/'+'*PlateSum*.fits'))
-                if len(plates) != 0:
-                    moonphase = np.empty(len(plates))
-                    for k in range(len(plates)):
-                        tmp = fits.open(plates[k])
-                        plsum1 = tmp[1].data
-                        moonphase[k] = np.mean(plsum1['MOONPHASE'])
-                    meanmoonphase = np.mean(moonphase)
-                    bgcolor = '#000000'
-                    txtcolor = '#FFFFFF'
-                    if meanmoonphase > 0.5: txtcolor = '#000000'
-                    if meanmoonphase > 0.1: bgcolor = '#282828'
-                    if meanmoonphase > 0.2: bgcolor = '#404040'
-                    if meanmoonphase > 0.3: bgcolor = '#606060'
-                    if meanmoonphase > 0.4: bgcolor = '#787878'
-                    if meanmoonphase > 0.5: bgcolor = '#989898'
-                    if meanmoonphase > 0.6: bgcolor = '#B0B0B0'
-                    if meanmoonphase > 0.7: bgcolor = '#C8C8C8'
-                    if meanmoonphase > 0.8: bgcolor = '#E8E8E8'
-                    if meanmoonphase > 0.9: bgcolor = '#FFFFFF'
-                    mphase = str(int(round(meanmoonphase*100)))+'%'
-                    html.write('<TD bgcolor="'+bgcolor+'" align="right" style = "color:'+txtcolor+';">'+mphase+'\n') 
-                else:
-                    bgcolor = '#000000'
-                    txtcolor = '#FFFFFF'
-                    meanmoonphase = moon_illumination(tt)
-                    if meanmoonphase > 0.5: txtcolor = '#000000'
-                    if meanmoonphase > 0.1: bgcolor = '#282828'
-                    if meanmoonphase > 0.2: bgcolor = '#404040'
-                    if meanmoonphase > 0.3: bgcolor = '#606060'
-                    if meanmoonphase > 0.4: bgcolor = '#787878'
-                    if meanmoonphase > 0.5: bgcolor = '#989898'
-                    if meanmoonphase > 0.6: bgcolor = '#B0B0B0'
-                    if meanmoonphase > 0.7: bgcolor = '#C8C8C8'
-                    if meanmoonphase > 0.8: bgcolor = '#E8E8E8'
-                    if meanmoonphase > 0.9: bgcolor = '#FFFFFF'
-                    mphase = str(int(round(meanmoonphase*100)))+'%'
-                    html.write('<TD bgcolor="'+bgcolor+'" align="right" style = "color:'+txtcolor+';">'+mphase+'\n') 
+                bgcolor = '#000000'
+                txtcolor = '#FFFFFF'
+                meanmoonphase = moon_illumination(tt)
+                if meanmoonphase > 0.5: txtcolor = '#000000'
+                if meanmoonphase > 0.1: bgcolor = '#282828'
+                if meanmoonphase > 0.2: bgcolor = '#404040'
+                if meanmoonphase > 0.3: bgcolor = '#606060'
+                if meanmoonphase > 0.4: bgcolor = '#787878'
+                if meanmoonphase > 0.5: bgcolor = '#989898'
+                if meanmoonphase > 0.6: bgcolor = '#B0B0B0'
+                if meanmoonphase > 0.7: bgcolor = '#C8C8C8'
+                if meanmoonphase > 0.8: bgcolor = '#E8E8E8'
+                if meanmoonphase > 0.9: bgcolor = '#FFFFFF'
+                mphase = str(int(round(meanmoonphase*100)))+'%'
+                html.write('<TD bgcolor="'+bgcolor+'" align="right" style = "color:'+txtcolor+';">'+mphase+'\n') 
         html.write('</table>\n')
 
         # Summary calibration data
