0cc9871e7ced510529dc7aa616839b390b46c0ab
drewchojnowski <drewski@nmsu.edu>
2020-12-08 13:12:12 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1642,7 +1642,7 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
                     rvlogg = str("%.3f" % round(allV['RV_LOGG'][gd][0],3))
                     rvfeh = str("%.3f" % round(allV['RV_FEH'][gd][0],3))
                     fcol = 'black'
-                    if np.absolute(vhelio) > 400: fcol = 'red'
+                    if np.absolute(allV['VHELIOBARY'][gd][0]) > 400: fcol = 'red'
                     objhtml.write('<FONT COLOR="'+fcol+'"+<TD align ="right">' + vhelio + '<TD align ="center">' + ncomp + '<TD align ="right">' + rvteff)
                     objhtml.write('<TD align ="right">' + rvlogg + '<TD align ="right">' + rvfeh + '</FONT>')
                 else:
