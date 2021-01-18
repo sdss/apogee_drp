b60c5393c0887a5af6530f457ff05bd726473c54
drewchojnowski <drewski@nmsu.edu>
2021-01-18 16:03:33 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -461,7 +461,8 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
         tcal = allcal[thar]
         tcaljd = tcal['JD']-2.4e6
 
-        flux = tcal['GAUSS'][:,:,0] * tcal['GAUSS'][:,:,2]**2
+        flux = tcal['GAUSS'][:,:,:,0] * tcal['GAUSS'][:,:,:,2]**2
+        import pdb; pdb.set_trace()
 
         for ichip in range(nchips):
             chip = chips[ichip]
