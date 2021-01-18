e201902256bd25f9b81b357fa96def9c2d2ec0f5
drewchojnowski <drewski@nmsu.edu>
2021-01-18 15:13:24 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -438,6 +438,8 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
             if ichip == 1: ax.set_ylabel(r'Median Flux')
             if ichip < nchips-1: ax.axes.xaxis.set_ticklabels([])
 
+            import pdb; pdb.set_trace()
+
             ax.scatter(qcaljd, qcal['FLUX'][10, ichip] / qcal['NREAD']*10.0, marker='o', s=60, color='b', alpha=alpha, label='Fiber 10')
             ax.scatter(qcaljd, qcal['FLUX'][150, ichip] / qcal['NREAD']*10.0, marker='^', s=60, color='g', alpha=alpha, label='Fiber 150')
             ax.scatter(qcaljd, qcal['FLUX'][290, ichip] / qcal['NREAD']*10.0, marker='*', s=100, color='r', alpha=alpha, label='Fiber 290')
