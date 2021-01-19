cebb83aa2d4e01fc44977c5a09cff2dae7c2bcc6
drewchojnowski <drewski@nmsu.edu>
2021-01-19 13:22:34 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -616,7 +616,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
     if (os.path.exists(plotfile) == False) | (clobber == True):
         print("----> monitor: Making " + plotfile)
 
-        fig = plt.figure(figsize=(28,5.5))
+        fig = plt.figure(figsize=(28,8))
         ymax = 20.8
         ymin = 10
 
@@ -637,7 +637,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
         jd = t.jd - 2.4e6
         ax.scatter(jd, allsci['ZERO'], marker='o', s=markersz, color='k', alpha=alf)
 
-        fig.subplots_adjust(left=0.04,right=0.99,bottom=0.12,top=0.98,hspace=0.08,wspace=0.00)
+        fig.subplots_adjust(left=0.045,right=0.99,bottom=0.12,top=0.98,hspace=0.08,wspace=0.00)
         plt.savefig(plotfile)
         plt.close('all')
 
