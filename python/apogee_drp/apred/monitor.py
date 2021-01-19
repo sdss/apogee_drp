1211f07cd14767db5a7853d3952b34e881286654
drewchojnowski <drewski@nmsu.edu>
2021-01-19 15:32:12 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -858,7 +858,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
             ax1.text(t.jd-2.4e6, ymax+yspan*0.025, str(int(round(year))), ha='center')
 
         ax1.scatter(caljd, allepsf['CENT'], marker='o', s=markersz*2, color='teal', alpha=alf)
-        ax2.scatter(allepsf['LN2LEVEL'], allepsf['CENT'], marker='o', s=markersz*2, color='teal', alpha=alf)
+        ax2.scatter(allepsf['LN2LEVEL'], allepsf['CENT'], marker='o', s=markersz*3, color='teal', alpha=alf)
 
         fig.subplots_adjust(left=0.06,right=0.995,bottom=0.06,top=0.96,hspace=0.15,wspace=0.00)
         plt.savefig(plotfile)
