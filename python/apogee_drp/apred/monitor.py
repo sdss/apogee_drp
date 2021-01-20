25be16cbe0ca24683d52e1c8af0ea7dc510d1956
drewchojnowski <drewski@nmsu.edu>
2021-01-20 09:56:28 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -870,7 +870,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
 
     ###############################################################################################
     # biasmean.png
-    colors1 = np.array(['dimgrey', 'salmon', 'mediumspringgreen'])
+    colors1 = np.array(['dimgrey', 'salmon', 'cornflowerblue'])
     plotfile = specdir5 + 'monitor/' + instrument + '/biasmean.png'
     if (os.path.exists(plotfile) == False) | (clobber == True):
         print("----> monitor: Making " + plotfile)
