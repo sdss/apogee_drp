6c8fae8777dbf99e3fecd24b1ead8d105326ee7e
drewchojnowski <drewski@nmsu.edu>
2021-01-21 14:36:41 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -533,8 +533,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Fal
     for i in range(len(pfiles)):
         gplot = pfiles[i].split(telescope + '/')[1].split('/')[-1]
         check, = glob.glob('flatflux/' + gplot)
-        if len(check) < 1:
-        subprocess.call(['scp', pfiles[i], sdir5 + 'flatflux/'])
+        if len(check) < 1: subprocess.call(['scp', pfiles[i], sdir5 + 'flatflux/'])
 
     ###############################################################################################
     # Set up some basic plotting parameters
