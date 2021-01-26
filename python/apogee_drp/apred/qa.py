9541f6da75e2dc6c8c059fdbb9f8efd29d4d0a57
drewchojnowski <drewski@nmsu.edu>
2021-01-26 13:45:54 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2070,7 +2070,7 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
 
                         #ax1.text(0.98, 0.90, str(contord), transform=ax1.transAxes, ha='right', va='top')
 
-                        fig.subplots_adjust(left=0.05,right=0.99,bottom=0.09,top=0.98,hspace=0.1,wspace=0.0)
+                        fig.subplots_adjust(left=0.045,right=0.99,bottom=0.07,top=0.98,hspace=0.1,wspace=0.0)
                         plt.savefig(starPlotFilePath)
                         plt.close('all')
                         print('done')
