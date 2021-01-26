707b8fe88a3a693f352d9cc4de7dbdb319f8ee80
drewchojnowski <drewski@nmsu.edu>
2021-01-26 13:21:04 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2021,7 +2021,7 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
                         print("\n----> makeObjQA: Making " + os.path.basename(starPlotFilePath))
 
                         hdr = fits.getheader(apStarPath)
-                        flux = fits.open(apStarPath)[1].data
+                        flux = fits.open(apStarPath)[1].data[0]
                         npix = len(flux)
                         wstart = hdr['CRVAL1']
                         wstep = hdr['CDELT1']
