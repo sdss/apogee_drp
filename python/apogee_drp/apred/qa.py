bc74b369b63015c1d0cc43d9e93c80a2b5c75262
drewchojnowski <drewski@nmsu.edu>
2021-01-26 13:54:49 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1726,7 +1726,7 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
                     apStarNewest = os.path.basename(apStarCheck[-1])
                     apStarRelPath = starRelPath + apStarNewest
                     apStarPath = starDir + apStarNewest
-                    apStarModelPath = starDir + apStarNewest.replace('.fits', '_out_doppler.pkl')
+                    apStarModelPath = apStarPath.replace('.fits', '_out_doppler.pkl')
 
                     # Set up plot directories and plot file name
                     starPlotDir = starDir + 'plots/'
