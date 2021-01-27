bbf46943c28262e78558d27ab843ea3ebc4997fd
drewchojnowski <drewski@nmsu.edu>
2021-01-27 14:42:58 -0700

    debugging peakfit

--- python/apogee_drp/utils/peakfit.py
+++ python/apogee_drp/utils/peakfit.py
@@ -118,7 +118,6 @@ def gausspeakfit(spec,pix0=None,estsig=5,sigma=None,func=gaussbin) :
         pars,cov = curve_fit(func,xx,y,p0=initpar,sigma=yerr,bounds=bounds,maxfev=1000)            
         perr = np.sqrt(np.diag(cov))
     except:
-        import pdb; pdb.set_trace()
         return None,None
     
     return pars,perr
