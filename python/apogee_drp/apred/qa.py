9265ca4a443222fb8ee4f153b9a0848cf4eb2931
drewchojnowski <drewski@nmsu.edu>
2021-01-27 12:33:25 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1943,8 +1943,8 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
                             print("----> makeObjQA: Making " + os.path.basename(starPlotFilePath))
 
                             contord = 5
-                            data = doppler.read(apStarPath)
-                            data.normalize()
+                            apstar = doppler.read(apStarPath)
+                            apstar.normalize()
                             wave = data.wave[:, 0]
                             flux = data.flux[:, 0]
                             gd, = np.where(np.isnan(flux) == False)
