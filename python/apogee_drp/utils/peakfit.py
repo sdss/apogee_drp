afd39abb686d51478ca388e24877f1fef803388d
drewchojnowski <drewski@nmsu.edu>
2021-01-27 14:25:10 -0700

    debugging peakfit

--- python/apogee_drp/utils/peakfit.py
+++ python/apogee_drp/utils/peakfit.py
@@ -115,8 +115,8 @@ def gausspeakfit(spec,pix0=None,estsig=5,sigma=None,func=gaussbin) :
     bounds = (lbounds,ubounds)
     # Sometimes curve_fit hits the maximum number fo function evaluations and crashes
     #try:
-    #    pars,cov = curve_fit(func,xx,y,p0=initpar,sigma=yerr,bounds=bounds,maxfev=1000)            
-    #    perr = np.sqrt(np.diag(cov))
+    pars,cov = curve_fit(func,xx,y,p0=initpar,sigma=yerr,bounds=bounds,maxfev=1000)            
+    perr = np.sqrt(np.diag(cov))
     #except:
     #    return None,None
     
