0769bc48e7ac155bc44c4b9863e57bd495099204
drewchojnowski <drewski@nmsu.edu>
2021-01-28 14:56:01 -0700

    updating peakfit.py

--- python/apogee_drp/utils/peakfit.py
+++ python/apogee_drp/utils/peakfit.py
@@ -199,7 +199,7 @@ def peakfit(spec,sigma=None,pix0=None):
             # Sum up the flux
             xlo1 = np.maximum(0,int(pars[1]-np.maximum(2.5*pars[2],3)))
             xhi1 = np.minimum(npix,int(pars[1]+np.maximum(2.5*pars[2],3)))
-            sumflux = np.sum(np.maximum(resid[xlo1:xhi1]-pars[3],0))
+            sumflux = np.sum(np.maximum(resid[xlo1:xhi1]-np.median(resid[xlo1:xhi1]),0))
 
             # Get model and subtract from residuals
             xlo = np.maximum(0,int(pars[1]-5*pars[2]))
