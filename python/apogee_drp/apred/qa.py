3d445e509d10b750fa537a989c55daeca94df718
drewchojnowski <drewski@nmsu.edu>
2021-01-29 13:51:59 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -148,9 +148,9 @@ def apqaMJD(mjd='59146', observatory='apo', apred='daily', makeplatesum=True, ma
         #print("Done with APQAMJD for " + str(ndarkplans) + " dark plans from MJD " + mjd + "\n")
 
         # Make the MJDexp fits file for this MJD
-        #print("Making " + mjd + "exp.fits\n")
-        #x = makeExpFits(instrument=instrument, apodir=apodir, apred=apred, load=load, mjd=mjd)
-        #print("Done with APQAMJD for " + str(ndarkplans) + " dark plans from MJD " + mjd + "\n")
+        print("Making " + mjd + "exp.fits\n")
+        x = makeExpFits(instrument=instrument, apodir=apodir, apred=apred, load=load, mjd=mjd)
+        print("Done with APQAMJD for " + str(ndarkplans) + " dark plans from MJD " + mjd + "\n")
 
     # Run apqa on the science data plans
     print("Running APQAMJD for " + str(nsciplans) + " plates observed on MJD " + mjd + "\n")
