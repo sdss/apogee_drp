4fee52eb158fb8b09588f89922d651d433259b91
drewchojnowski <drewski@nmsu.edu>
2021-01-30 12:24:49 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -126,26 +126,7 @@ def apqaMJD(mjd='59146', observatory='apo', apred='daily', makeplatesum=True, ma
     ndarkplans = len(darkplans)
 
     if makeqafits is True:
-        # Run apqa on the cal  plans
-        print("Running APQAMJD for " + str(ncalplans) + " cal plans from MJD " + mjd + "\n")
-        for i in range(ncalplans): 
-            planfile = load.filename('CalPlan', mjd=mjd)
-            planstr = plan.load(planfile, np=True)
-            mjd = calplans[i].split('-')[3].split('.')[0]
-            all_ims = planstr['APEXP']['name']
-            x = makeCalFits(load=load, ims=all_ims, mjd=mjd, instrument=instrument, clobber=clobber)
-        print("Done with APQAMJD for " + str(ncalplans) + " cal plans from MJD " + mjd + "\n")
 
-        # Run apqa on the dark  plans
-        print("Running APQAMJD for " + str(ndarkplans) + " dark plans from MJD " + mjd + "\n")
-        for i in range(ndarkplans): 
-            planfile = load.filename('DarkPlan', mjd=mjd)
-            print(planfile)
-            planstr = plan.load(planfile, np=True)
-            mjd = darkplans[i].split('-')[3].split('.')[0]
-            all_ims = planstr['APEXP']['name']
-            x = makeDarkFits(load=load, ims=all_ims, mjd=mjd, clobber=clobber)
-        print("Done with APQAMJD for " + str(ndarkplans) + " dark plans from MJD " + mjd + "\n")
 
         # Make the MJDexp fits file for this MJD
         print("Making " + mjd + "exp.fits\n")
