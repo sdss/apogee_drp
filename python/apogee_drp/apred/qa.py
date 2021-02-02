1afe43f53ee70b433618ba626c5c8325ac41fada
drewchojnowski <stephen.chojnowski@montana.edu>
2021-02-02 12:43:17 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2052,7 +2052,7 @@ def makeObjQA(load=None, plate=None, mjd=None, survey=None, apred=None, telescop
 
                                 ichip += 1
 
-                            txt = objid+',  H = '+chmag+',  '+str(nvis)+' visits'
+                            txt = objid + r',  $H$ = ' + chmag + ',  ' + str(nvis) + ' visits'
                             ax1.text(0.5, 0.05, txt, transform=ax1.transAxes, bbox=bboxpar, ha='center', fontsize=fontsize*1.25)
                             txt = r'$T_{\rm eff}$ = ' + rvteff + ' K,    log(g) = ' + rvlogg + ',    [Fe/H] = '+rvfeh
                             ax2.text(0.5, 0.05, txt, transform=ax2.transAxes, bbox=bboxpar, ha='center', fontsize=fontsize*1.25)
