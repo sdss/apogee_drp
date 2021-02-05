0c40565895df115f4e431be2123f236d1351f10d
drewchojnowski <stephen.chojnowski@montana.edu>
2021-02-05 12:40:15 -0700

    trying to speed up qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1203,7 +1203,7 @@ def makeObsPlots(load=None, ims=None, imsReduced=None, plate=None, mjd=None, ins
             tput = med[ypos] / np.nanmax(med[ypos])
             gd, = np.where((ypos > 60) & (ypos < 90))
             print(np.nanmean(tput[gd]))
-            sc = ax.scatter(platesum2['Zeta'], platesum2['Eta'], marker='o', s=100, c=tput, edgecolors='k', cmap='Reds', alpha=1, vmin=0.0, vmax=1.0)
+            sc = ax.scatter(platesum2['Zeta'], platesum2['Eta'], marker='o', s=100, c=tput, edgecolors='k', cmap='Reds_r', alpha=1, vmin=0.0, vmax=1.0)
 
             ax.text(0.03, 0.97, chiplab[ichip]+'\n'+'chip', transform=ax.transAxes, ha='left', va='top', color=chiplab[ichip])
 
