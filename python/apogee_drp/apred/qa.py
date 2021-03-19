3c4d8abb8624f2069ddc450e7fb4459b0d093a56
drewchojnowski <drewski@nmsu.edu>
2021-03-19 13:47:50 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2352,7 +2352,7 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
                 ax1.text(0.5, 1.02, txt1 + txt2, transform=ax1.transAxes, ha='center', fontsize=fontsize*1.25, color='k')#, bbox=bboxpar)
                 #ax2.legend(loc='upper left', edgecolor='k', ncol=2, fontsize=fontsize*1.25, framealpha=0.8)
 
-                fig.subplots_adjust(left=0.043,right=0.99,bottom=0.05,top=0.96,hspace=-0.05,wspace=0.0)
+                fig.subplots_adjust(left=0.043,right=0.99,bottom=0.05,top=0.96,hspace=-0.03,wspace=0.0)
                 plt.savefig(starPlotFilePath)
                 plt.close('all')
 
