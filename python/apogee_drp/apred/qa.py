0793d3f313151aa9d8c6ee631b284ffd5ad2ef6f
drewchojnowski <drewski@nmsu.edu>
2021-03-19 13:28:40 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2340,7 +2340,7 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
 
                 txt1 = objid + r'          $H$ = ' + chmag + '          ' + str(nvis) + ' visits          '
                 txt2 = r'$T_{\rm eff}$ = ' + rvteff + ' K          log(g) = ' + rvlogg + '          [Fe/H] = '+rvfeh
-                ax1.text(0.5, 1.02, txt1 + txt2, transform=ax1.transAxes, bbox=bboxpar, ha='center', fontsize=fontsize*1.25, color='k')
+                ax1.text(0.5, 1.02, txt1 + txt2, transform=ax1.transAxes, ha='center', fontsize=fontsize*1.25, color='k')#, bbox=bboxpar)
                 #ax2.legend(loc='upper left', edgecolor='k', ncol=2, fontsize=fontsize*1.25, framealpha=0.8)
 
                 fig.subplots_adjust(left=0.043,right=0.99,bottom=0.05,top=0.96,hspace=0.11,wspace=0.0)
