b4041f167f8122d089f005d423d78d095c79994d
drewchojnowski <drewski@nmsu.edu>
2021-03-19 14:37:33 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2299,10 +2299,15 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
                 #sflux = bestmodel.flux
                 swave = np.concatenate([bestmodel.wave[:, 0], bestmodel.wave[:,1], bestmodel.wave[:,2]])
                 sflux = np.concatenate([bestmodel.flux[:, 0], bestmodel.flux[:,1], bestmodel.flux[:,2]])
-                Word = np.argsort(swave)
-                swave = swave[Word]
-                sflux = sflux[Word]
-                import pdb; pdb.set_trace()
+                Worder = np.argsort(swave)
+                swave = swave[Worder]
+                sflux = sflux[Worder]
+                sfluxg = f(wave[g])
+                f = interpolate.interp1d(swave, sflux)
+                swaveg = np.linspace(wmin, wmax, nwave)
+                sfluxg = f(wave[g])
+                resid = sfluxg - flux[g]
+
                 rvteff = str(int(round(sumstr['teff'][0])))
                 rvlogg = str("%.3f" % round(sumstr['logg'][0],3))
                 rvfeh = str("%.3f" % round(sumstr['feh'][0],3))
@@ -2337,19 +2342,19 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
                     if ii % 2 == 1: ax.axhline(y=0, linestyle='dashed', linewidth=lwidth, color='k')
                     if ii % 2 == 0: ax.axes.xaxis.set_ticklabels([])
 
-                    g, = np.where((wave >= xmin[ichip] - 20) & (wave <= xmax[ichip] + 20))
-                    wmin = np.min(wave[g]); wmax = np.max(wave[g]); nwave = len(g)
-                    f = interpolate.interp1d(swave, sflux)
-                    swaveg = np.linspace(wmin, wmax, nwave)
-                    if j+1 == 5:
-                        import pdb; pdb.set_trace()
-                    sfluxg = f(wave[g])
+                    #g, = np.where((wave >= xmin[ichip] - 20) & (wave <= xmax[ichip] + 20))
+                    #wmin = np.min(wave[g]); wmax = np.max(wave[g]); nwave = len(g)
+                    #f = interpolate.interp1d(swave, sflux)
+                    #swaveg = np.linspace(wmin, wmax, nwave)
+                    #if j+1 == 5:
+                    #    import pdb; pdb.set_trace()
+                    #sfluxg = f(wave[g])
                     
                     if ii % 2 == 0: 
                         ax.plot(wave[g], flux[g], color='k', label='apStar')
                         ax.plot(wave[g], sfluxg, color='r', label='Cannon model', alpha=0.75)
                     else:
-                        resid = sfluxg - flux[g]
+                        #resid = sfluxg - flux[g]
                         ax.plot(swaveg, resid, color='b', alpha=0.75)
 
                     if ii % 2 == 1: ichip += 1
