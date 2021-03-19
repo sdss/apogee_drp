b33acacaa7c424541f9d735d9a3d4f41f42f2e8f
drewchojnowski <drewski@nmsu.edu>
2021-03-19 13:43:00 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2318,7 +2318,7 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
                 ichip = 0
                 for ax in axes:
                     ax.set_xlim(xmin[ichip], xmax[ichip])
-                    if ii % 2 == 0: ax.set_ylim(0.2, 1.3)
+                    if ii % 2 == 0: ax.set_ylim(0.1, 1.3)
                     if ii % 2 == 1: ax.set_ylim(-0.2, 0.2)
                     ax.tick_params(reset=True)
                     ax.xaxis.set_major_locator(ticker.MultipleLocator(50))
@@ -2352,7 +2352,7 @@ def apStarPlots(load=None, plate=None, mjd=None, apred=None, telescope=None):
                 ax1.text(0.5, 1.02, txt1 + txt2, transform=ax1.transAxes, ha='center', fontsize=fontsize*1.25, color='k')#, bbox=bboxpar)
                 #ax2.legend(loc='upper left', edgecolor='k', ncol=2, fontsize=fontsize*1.25, framealpha=0.8)
 
-                fig.subplots_adjust(left=0.043,right=0.99,bottom=0.05,top=0.96,hspace=0.11,wspace=0.0)
+                fig.subplots_adjust(left=0.043,right=0.99,bottom=0.05,top=0.96,hspace=0.05,wspace=0.0)
                 plt.savefig(starPlotFilePath)
                 plt.close('all')
 
