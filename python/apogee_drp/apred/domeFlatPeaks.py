919d03c43396914e555dc08dfff0dbe9e9a46401
drewchojnowski <drewski@nmsu.edu>
2021-03-30 14:32:48 -0600

    developing domeFlatPeaks.py

--- python/apogee_drp/apred/domeFlatPeaks.py
+++ python/apogee_drp/apred/domeFlatPeaks.py
@@ -193,6 +193,7 @@ def FindAllPeaks2(apred='daily', telescope='apo25m', medianrad=200, ndomes=None)
 
     exp = fits.getdata(mdir + instrument + 'Exp.fits')
     gd, = np.where(exp['IMAGETYP'] == 'DomeFlat')
+    gd, = np.where((exp['IMAGETYP'] == 'DomeFlat') & (exp['MJD'] > 57000))
     exp = exp[gd]
     if ndomes is None: ndomes = len(gd)
     ndomestr = str(ndomes)
