f8916bdc89a6f34eacdd56f2d113020c604b6d03
drewchojnowski <drewski@nmsu.edu>
2021-03-31 14:08:42 -0600

    developing domeFlatPeaks.py

--- python/apogee_drp/apred/domeFlatPeaks.py
+++ python/apogee_drp/apred/domeFlatPeaks.py
@@ -331,10 +331,12 @@ def matchtrace(apred='daily', telescope='apo25m', medianrad=100, ndomes=None, ex
         for idome in range(ndomes):
             diff = np.absolute(dcent[idome] - gpeaks['pars'][:, 1])
             gd, = np.where((np.isnan(diff) == False) & (diff < 1))
+            if len(gd) < 5: continue
             diff = diff[gd]
             ndiff = len(diff)
             rms[ichip, idome] = np.sqrt(np.sum(diff**2)/ndiff)
-            import pdb; pdb.set_trace()
+
+    import pdb; pdb.set_trace()
         
         
         #for i in range(len(dome)):
