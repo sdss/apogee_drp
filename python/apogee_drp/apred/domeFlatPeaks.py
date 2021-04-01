3131701d66b5d8998b18a34691df07a03ec1a42c
drewchojnowski <drewski@nmsu.edu>
2021-04-01 10:12:22 -0600

    developing domeFlatPeaks.py

--- python/apogee_drp/apred/domeFlatPeaks.py
+++ python/apogee_drp/apred/domeFlatPeaks.py
@@ -382,6 +382,7 @@ def plotresid(apred='daily', telescope='apo25m', medianrad=100, expnum=36760022)
     axmajlen=7
     axminlen=3.5
     colors = np.array(['red','green','blue'])
+    markers = np.array(['o','^','s'])
 
     plotfile = 'dflatTrace_sci' + str(expnum) + '_dome' + str(psfid) + '.png'
     fig=plt.figure(figsize=(20,8))
@@ -424,8 +425,8 @@ def plotresid(apred='daily', telescope='apo25m', medianrad=100, expnum=36760022)
         gd, = np.where(np.isnan(diff) == False)
         num = gpeaks['num'][gd]
         gdiff = diff[gd]
-        ax.scatter(num, gdiff, marker='o', color='white', edgecolors=colors[ichip], linewidth=1.5, alpha=0.5)
-        ax.scatter(gnum, diff[gdd], marker='o', color=colors[ichip], linewidth=1.5, alpha=1)
+        ax.scatter(num, gdiff, marker=markers[ichip], color='white', edgecolors=colors[ichip], linewidth=1.5, alpha=0.5)
+        ax.scatter(gnum, diff[gdd], marker=markers[ichip], color=colors[ichip], linewidth=1.5, alpha=1)
 
     fig.subplots_adjust(left=0.06,right=0.99,bottom=0.09,top=0.98,hspace=0.09,wspace=0.04)
     plt.savefig(mdir + plotfile)
