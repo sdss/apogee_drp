bebfc9cd1d69d8c3909f660fa6d97865fc759776
drewchojnowski <drewski@nmsu.edu>
2021-04-07 16:04:08 -0600

    updating domeFlatPeaks.py

--- python/apogee_drp/apred/domeFlatPeaks.py
+++ python/apogee_drp/apred/domeFlatPeaks.py
@@ -203,9 +203,9 @@ def matchtrace(apred='daily', telescope='apo25m', medianrad=100, expnum=36760022
 
 
 
-    rmsSum = np.nansum(rms, axis=0)
+    rmsSum = np.nanmean(rms, axis=0)
     gd, = np.where(rmsSum == np.nanmin(rmsSum))
-    print(rms[ichip, idome[gd][0]])
+    print(rms[ichip, idome[gd]])
 
     print(' Best dome flat for exposure ' + str(expnum) + ': ' + str(dome['PSFID'][gd][0]) + ' (rmsSum = ' + str(rmsSum[gd][0]) + ')')
     runtime = str("%.2f" % (time.time() - start_time))
