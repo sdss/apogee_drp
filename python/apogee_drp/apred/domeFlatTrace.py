00279c505f099a781e523e72a617db1edbe7b302
drewchojnowski <drewski@nmsu.edu>
2021-06-11 12:43:15 -0600

    updating domeFlatTrace.py

--- python/apogee_drp/apred/domeFlatTrace.py
+++ python/apogee_drp/apred/domeFlatTrace.py
@@ -222,7 +222,6 @@ def findBestFlatExposure(apred='daily', telescope='apo25m', medianrad=100, expnu
             ndiff = len(diff)
             rms[ichip, idome] = np.sqrt(np.nansum(diff**2)/ndiff)
             if silent is False: print("   Final match based on " + str(ndiff) + " fibers:")
-            if silent is False: print(gpeaks['num'][gd])
 
     rmsMean = np.nanmean(rms, axis=0)
     gd, = np.where(rmsMean == np.nanmin(rmsMean))
