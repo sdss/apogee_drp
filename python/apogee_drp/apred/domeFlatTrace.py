fafb56d5dc23b2d33637a8a78ce952917ddd25f3
drewchojnowski <drewski@nmsu.edu>
2021-06-11 13:42:41 -0600

    updating domeFlatTrace.py

--- python/apogee_drp/apred/domeFlatTrace.py
+++ python/apogee_drp/apred/domeFlatTrace.py
@@ -50,8 +50,17 @@ def findBestFlatSequence(ims=None, planfile=None, plate='15000', mjd='59146', te
     start_time = time.time()
     #print("Finding best dome flats for plate " + plate + ", MJD " + mjd)
 
+    load = apload.ApLoad(apred=apred, telescope=telescope)
+
     instrument = 'apogee-n'
-    if telescope == 'lco25m': instrument = 'apogee-s'
+    inst = 'N'
+    if telescope == 'lco25m':
+        instrument = 'apogee-s'
+        inst = 'S'
+    fil = os.path.abspath(__file__)
+    codedir = os.path.dirname(fil)
+    datadir = os.path.dirname(os.path.dirname(os.path.dirname(codedir))) + '/data/domeflat/'
+    refpix = ascii.read(datadir + 'refpix' + inst + '.dat')
 
     # Establish directories.
     datadir = {'apo25m':os.environ['APOGEE_DATA_N'], 'apo1m':os.environ['APOGEE_DATA_N'],
@@ -60,12 +69,11 @@ def findBestFlatSequence(ims=None, planfile=None, plate='15000', mjd='59146', te
     mdir = apodir + 'monitor/'
 
     # Read in the dome flat lookup table and master exposure table
-    dome = fits.getdata(mdir + instrument + 'DomeFlatTrace-all.fits')
-    exp = fits.getdata(mdir + instrument + 'Exp.fits')
+    domeTable = fits.getdata(mdir + instrument + 'DomeFlatTrace-all.fits')
+    expTable = fits.getdata(mdir + instrument + 'Exp.fits')
 
     if ims is None:
-        # Use telescope, plate, mjd, and apred to load planfile into structure.
-        load = apload.ApLoad(apred=apred, telescope=telescope)
+        # Load planfile into structure.
         if planfile is None: planfile = load.filename('Plan', plate=int(plate), mjd=mjd)
         planstr = plan.load(planfile, np=True)
 
@@ -73,26 +81,8 @@ def findBestFlatSequence(ims=None, planfile=None, plate='15000', mjd='59146', te
         gd, = np.where(planstr['APEXP']['flavor'] == 'object')
         ims = planstr['APEXP']['name'][gd]
         mjd = str(planstr['mjd'])
-
-        #if n_ims > 0:
-        #    ims = all_ims[gd]
-        #    # Make an array indicating which exposures made it to apCframe
-        #    # 0 = not reduced, 1 = reduced
-        #    imsReduced = np.zeros(n_ims)
-        #    for i in range(n_ims):
-        #        file2d = load.filename('2D', mjd=mjd, num=ims[i], chips='c').replace('2D-', '2D-c-')
-        #        #twodname = load.filename('2D', num=ims[i], chips=True)
-        #        if os.path.exists(file2d): imsReduced[i] = 1
-        #    good, = np.where(imsReduced == 1)
-        #    if len(good) > 0:
-        #        ims = ims[good]
-        #    else:
-        #        print("PROBLEM!!! 2D files not found . Skipping.\n")
-        #        return
-        #else:
-        #    print("PROBLEM!!! No object images found. Skipping.\n")
-        #    return
     else:
+        # Construct MJD from exposure number
         num = (ims[0] - ims[0] % 10000 ) / 10000
         mjd = '{:05d}'.format(int(num) + 55562)
 
@@ -104,15 +94,21 @@ def findBestFlatSequence(ims=None, planfile=None, plate='15000', mjd='59146', te
     rms =       np.empty(n_ims)
 
     for i in range(n_ims):
+        # Find the ap2D files for this exposure
+        file2d = load.filename('2D', mjd=mjd, num=expnum, chips='c')
+        twodfiles = np.array([file2d.replace('2D-', '2D-a-'),
+                              file2d.replace('2D-', '2D-b-'),
+                              file2d.replace('2D-', '2D-c-')])
+
         # Run findBestFlatExposure on this exposure
-        dflatnums[i], dflatmjds[i], rms[i] = findBestFlatExposure(apred=apred, telescope=telescope, mjd=mjd, expnum=ims[i],
+        dflatnums[i], dflatmjds[i], rms[i] = findBestFlatExposure(domeTable=domeTable, refpix=refpix, twodfiles=twodfiles, medianrad=medianrad,
                                                                   minflux=minflux, highfluxfrac=highfluxfrac, silent=silent)
-        # Get info about exposure and matching dome flat
-        pflat, = np.where(dflatnums[i] == dome['PSFID'])
-        psci, = np.where(ims[i] == exp['NUM'])
+        # Print info about this exposure and the matching dome flat
+        pflat, = np.where(dflatnums[i] == domeTable['PSFID'])
+        psci, = np.where(ims[i] == expTable['NUM'])
         p1 = '(' + str(i+1).rjust(2) + ') sci exposure ' + str(ims[i]) + ' ----> dflat ' + str(int(round(dflatnums[i]))) + ' (MJD ' + str(int(round(dflatmjds[i]))) + '),  '
-        p2 = 'alt [' + str("%.3f" % round(exp['ALT'][psci][0],3)) + ', ' + str("%.3f" % round(dome['ALT'][pflat][0],3)) + '],  '
-        p3 = 'ln2level [' + str("%.3f" % round(exp['LN2LEVEL'][psci][0],3)) + ', ' + str("%.3f" % round(dome['LN2LEVEL'][pflat][0],3)) + '],   '
+        p2 = 'alt [' + str("%.3f" % round(expTable['ALT'][psci][0],3)) + ', ' + str("%.3f" % round(domeTable['ALT'][pflat][0],3)) + '],  '
+        p3 = 'ln2level [' + str("%.3f" % round(expTable['LN2LEVEL'][psci][0],3)) + ', ' + str("%.3f" % round(domeTable['LN2LEVEL'][pflat][0],3)) + '],   '
         p4 = 'rms = ' + str("%.4f" % round(rms[i],4))
         print(p1 + p2 + p3 + p4)
 
@@ -141,51 +137,25 @@ def findBestFlatSequence(ims=None, planfile=None, plate='15000', mjd='59146', te
 # Output is the matched dome flat exposure number, MJD, and the r.m.s. of the match.
 #
 ###################################################################################################
-def findBestFlatExposure(apred='daily', telescope='apo25m', medianrad=100, mjd='59249', expnum=36870054, 
-                         minflux=None, highfluxfrac=None, silent=True):
+def findBestFlatExposure(domeTable=None, refpix=None, twodfiles=None, medianrad=100, minflux=None, highfluxfrac=None, silent=True):
 
-    start_time = time.time()
-
-    chips = np.array(['a','b','c'])
+    chips = np.array(['a', 'b', 'c'])
     nchips = len(chips)
     nfibers = 300
+    ndomes = len(domeTable)
 
-    instrument = 'apogee-n'
-    inst = 'N'
-    if telescope == 'lco25m':
-        instrument = 'apogee-s'
-        inst = 'S'
-    fil = os.path.abspath(__file__)
-    codedir = os.path.dirname(fil)
-    datadir = os.path.dirname(os.path.dirname(os.path.dirname(codedir))) + '/data/domeflat/'
-    refpix = ascii.read(datadir + 'refpix' + inst + '.dat')
-    
     apodir = os.environ.get('APOGEE_REDUX') + '/' + apred + '/'
     mdir = apodir + 'monitor/'
     expdir = apodir + 'exposures/' + instrument + '/'
 
-    dome = fits.getdata(mdir + instrument + 'DomeFlatTrace-all.fits')
-    ndomes = len(dome)
-
-    file2d = load.filename('2D', mjd=mjd, num=expnum, chips='c')
-    twodfiles = np.array([file2d.replace('2D-', '2D-a-'),
-                          file2d.replace('2D-', '2D-b-'),
-                          file2d.replace('2D-', '2D-c-')])
-
-    #twodFiles = glob.glob(expdir + '*/ap2D-*' + str(expnum) + '.fits')
-    #twodFiles.sort()
-    #twodFiles = np.array(twodFiles)
-
-    #if len(twodFiles) < 3:
-    #    if silent is False: print('PROBLEM: less then 3 ap2D files found for exposure ' + str(expnum))
-    #else:
-   #     if silent is False: print('ap2D files found for exposure ' + str(expnum))
-
     # Loop over the chips
     rms = np.full([nchips, ndomes], 50).astype(np.float64)
     for ichip in range(nchips):
+        twodfile = twodFiles[ichip]
+        expnum = int(twodfile.split('-')[:-1].split('.')[0])
+        import pdb; pdb.set_trace()
         pix0 = np.array(refpix[chips[ichip]])
-        gpeaks = gaussFitAll(infile=twodFiles[ichip], medianrad=medianrad, pix0=pix0)
+        gpeaks = gaussFitAll(infile=twodFile, medianrad=medianrad, pix0=pix0)
 
         # Remove failed and discrepant peakfits
         gd, = np.where(gpeaks['success'] == True)
@@ -234,7 +204,7 @@ def findBestFlatExposure(apred='daily', telescope='apo25m', medianrad=100, mjd='
     if silent is False: print("   rms:  " + str(rms[:, gd[0]]))
 
     gdrms = str("%.5f" % round(rmsMean[gd][0],5))
-    if silent is False: print("   Best dome flat for exposure " + str(expnum) + ": " + str(dome['PSFID'][gd][0]) + " (<rms> = " + str(gdrms) + ")")
+    if silent is False: print("   Best dome flat for exposure " + str(expnum) + ": " + str(domeTable['PSFID'][gd][0]) + " (<rms> = " + str(gdrms) + ")")
     runtime = str("%.2f" % (time.time() - start_time))
     if silent is False: print("\n   Done in " + runtime + " seconds.\n")
     
