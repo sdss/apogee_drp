d1fe9c3bc640da4df12acbb180a4162dcbc49dfd
DrewChojnowski <drewski@nmsu.edu>
2021-07-15 11:39:34 -0600

    updating domeFlatTrace.py

--- python/apogee_drp/apred/domeFlatTrace.py
+++ python/apogee_drp/apred/domeFlatTrace.py
@@ -207,9 +207,9 @@ def findBestFlatExposure(domeTable=None, refpix=None, twodfiles=None, medianrad=
         ngpeaks = len(gd)
 
 	    # Find median gaussian FWHM and restrict lookup table to similar values
-        medFWHM = np.nanmean(gpeaks['pars'][:, 2])*2.354
-        medDomeFWHM = np.nanmean(domeTable['GAUSS_SIGMA'][:, ichip, gpeaks['num']], axis=1)*2.354
-        gd, = np.where(np.absolute(medFWHM - medDomeFWHM) < 0.1)
+        medFWHM = np.nanmedian(gpeaks['pars'][:, 2])*2.354
+        medDomeFWHM = np.nanmedian(domeTable['GAUSS_SIGMA'][:, ichip, gpeaks['num']], axis=1)*2.354
+        gd, = np.where(np.absolute(medFWHM - medDomeFWHM) < 0.05)
         domeTable1 = domeTable[gd]
         ndomes1 = len(domeTable1)
         #pdb.set_trace()
