66754e24444ab5f971a3339cfae7d4f8f646e80c
David Nidever <dnidever@montana.edu>
2021-08-04 17:54:34 -0600

    Finished fpi.dailyfpiwave()

--- python/apogee_drp/apred/fpi.py
+++ python/apogee_drp/apred/fpi.py
@@ -16,6 +16,7 @@ import matplotlib
 import os
 import glob
 import pdb
+import time
 #from functools import wraps
 from astropy.io import ascii, fits
 #from scipy import signal, interpolate
@@ -27,8 +28,10 @@ from ..plan import mkplan
 from . import wave
 #from holtztools import plots, html
 from astropy.table import Table,hstack,vstack
-from dlnpyutils import utils as dln
+from dlnpyutils import utils as dln, robust
 matplotlib.use('Qt5Agg')
+
+chips = ['a','b','c']
 
 def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True):
     """
@@ -36,6 +39,10 @@ def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True
     arclamp data simultaneously fit with "apmultiwavecal" then we use the FPI full-frame exposure
     to get the FPI wavelengths and redo the wavelength solution.
     """
+
+    t0 = time.time()
+
+    print('Getting daily FPI wavelengths for MJD='+str(mjd5))
     
     reduxdir = os.environ['APOGEE_REDUX']+'/'+apred+'/'
     datadir = {'apo':os.environ['APOGEE_DATA_N'],'lco':os.environ['APOGEE_DATA_S']}[observatory]
@@ -86,8 +93,8 @@ def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True
     # This is what apmultiwavecal does
     if verbose:
         print('Solving wavelength solutions simultaneously using all arclamp exposures')
-    wfile = reduxdir+'cal/'+instrument+'/wave/apWave-%08d.fits' % mjd5
-    #wfile = reduxdir+'cal/'+instrument+'/wave/apWave-%s.fits' % str(mjd5)
+    #wfile = reduxdir+'cal/'+instrument+'/wave/apWave-%08d.fits' % mjd5
+    wfile = reduxdir+'cal/'+instrument+'/wave/apWave-%s.fits' % str(mjd5)
     #import pdb; pdb.set_trace()
     if os.path.exists(wfile.replace('apWave-','apWave-b-')) is False or clobber is True:
         # The previously measured lines in the apLines files will be reused if they exist
@@ -112,18 +119,46 @@ def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True
     #waveid = os.path.basename(dln.first_el(wavefiles))[9:17]
     #print('Using wavelength cal file ',waveid)
     #wavecal = load.apWave(waveid)  # why doesn't this work??
-    wfile = load.allfile('Wave',num=str(mjd5),chips=True)
+    #wfile = load.allfile('Wave',num=str(mjd5),chips=True)
     print('Using ',wfile,' wavelength calibration file')
     wavecal = load._readchip(wfile,'Wave')
-    
+
     # The wavelength solutions are fit in groups
     # find the group for the beginning of this night
+    ftable = wavecal['a'][7].data
     wheader = wavecal['a'][0].header
     nframes = wheader['nframes']
     ngroups = wheader['ngroup']
-    frames = []
-    for f in range(nframes):
-        frames.append(wheader['frame'+str(f)])
+    mjdframeind, = np.where(ftable['frame'].find(str(daynum)) > -1)
+
+
+    ######  MAKE SURE WE GET THE WAVLENGTH SOLUTION AT THE SAME DITHER POSITION AS THE FPI EXPOSURE #####
+
+    print('KLUDGE!!! setting frameind=0')
+    mjdframeind = np.array([0])
+    if len(mjdframeind)==0:
+        raise Exception('No frames for MJD='+str(mjd5))
+    # pick the group closest to the FPI exposure
+    bestind = np.argmin(np.abs(np.array(ftable['frame'][mjdframeind]).astype(int)-int(fpinum)))
+    group = ftable['group'][mjdframeind[bestind]]
+    # now get the wavelength solution parameters for this group
+    #  allpars has shape [npoly+3*ngroup,300]
+    #  the first parameters are the polynomial coefficients, and then
+    #  there are three offset values per group [chip1 offset ~ chipgap1, chip2 offset ~ 0, chip3 offset ~ chipgap2]
+    npoly = 4
+    allpars = wavecal['a'][6].data
+    wpars = np.zeros((npoly+3,300),float)
+    wpars[0:npoly,:] = allpars[0:npoly,:]
+    wpars[npoly:,:] = allpars[npoly+group*3:npoly+(group+1)*3,:]
+    # Now calculate the 2D wavelength arrays
+    waves = np.zeros((3,300,2048),float)
+    for row in range(300):
+        x = np.zeros([3,2048])
+        for ichip,chip in enumerate(chips):
+            x[0,:] = np.arange(2048)
+            x[1,:] = ichip+1
+            x[2,:] = 0
+            waves[ichip,row,:] = wave.func_multi_poly(x,*wpars[:,row],npoly=npoly)
 
     # Step 3: Fit peaks to the full-frame FPI data
     # --------------------------------------------
@@ -137,7 +172,7 @@ def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True
         fpilines = fitlines(fpiframe)
         # Save the catalog
         print('Writing FPI lines to ',fpilinesfile)
-        fpicat.write(fpilinesfile,overwrite=True)
+        fpilines.write(fpilinesfile,overwrite=True)
 
     # Step 4: Determine median wavelength per FPI lines
     # -------------------------------------------------
@@ -148,22 +183,22 @@ def dailyfpiwave(mjd5,observatory='apo',apred='daily',clobber=False,verbose=True
     else:
         print('No initial FPI peaks file found')
         fpipeaks = None
-    fpilinestr, fpilines = getfpiwave(fpilines,wavecal,fpipeaks)
+    fpilinestr, fpilines = getfpiwave(fpilines,wpars,fpipeaks)
 
     # Step 5: Refit wavelength solutions using FPI lines
     # --------------------------------------------------
-    fpiwavecal = fpiwavesol(fpilinestr,fpilines,wavecal)
-
-    import pdb; pdb.set_trace()
-
+    fpiwcoef,fpiwaves = fpiwavesol(fpilinestr,fpilines,wpars)
 
     # Save the results
     #-----------------
-    save_fpiwave(mjd5,fpiwavecal,fpilinestr,wavecal)
+    fpiwavefile = reduxdir+'cal/'+instrument+'/wave/apWaveFPI-%5d-%8d.fits' % (mjd5,fpinum)
+    print('Writing new FPI wavelength information to '+fpiwavefile)
+    save_fpiwave(fpiwavefile,mjd5,fpinum,fpiwcoef,fpiwaves,fpilinestr)
     # table of FPI lines data: chip, gauss center, Gaussian parameters, wavelength, flux
     # wavelength coefficients
     # wavelength array??
 
+    print("elapsed: %0.1f sec." % (time.time()-t0))
 
 
 
@@ -209,11 +244,11 @@ def fitlines(frame,verbose=False):
     return linestr
 
 
-def getfpiwave(fpilines,wavecal,fpipeaks,verbose=True):
+def getfpiwave(fpilines,wcoef,fpipeaks,verbose=True):
     """
     Determine median wavelength for the FPI lines
     fpilines: catalog of detected FPI peaks in full-frame image
-    wavecal: wavelength solution
+    wcoef: wavelength solution parameters [7,300]
     fpipeaks: fiducial wavelengths for the unique FPI peaks
     """
 
@@ -231,7 +266,7 @@ def getfpiwave(fpilines,wavecal,fpipeaks,verbose=True):
     fpilines['wave'] = 999999.        # wavelength from the arclamp wavelength solution
     fpilines['linewave'] = 999999.    # median wavelength of the FPI line
     fpilines['lineid'] = -1           # FPI line ID
-    wcoef = wavecal['a'][3].data  # [7,300], same for all three chips
+    #wcoef = wavecal['a'][3].data  # [7,300], same for all three chips
     chipnum = np.zeros(len(fpilines),int)
     for ichip,chip in enumerate(['a','b','c']):
         ind, = np.where(fpilines['chip']==chip)
@@ -258,7 +293,7 @@ def getfpiwave(fpilines,wavecal,fpipeaks,verbose=True):
         ind, = np.where(fpipeaks['CHIP']==chip)
         fpipeaks1 = fpipeaks[ind]
         fpilinestr1 = fpilinestr[ind]
-        wavecal1 = wavecal[chip]
+        #wavecal1 = wavecal[chip]
         lineind, = np.where(fpilines['chip']==chip)
         fpilines1 = fpilines[lineind]
         # FPI lines loop
@@ -294,13 +329,20 @@ def getfpiwave(fpilines,wavecal,fpipeaks,verbose=True):
 
     return fpilinestr, fpilines
 
-def fpiwavesol(fpilinestr,fpilines,wavecal,verbose=True):
+def fpiwavesol(fpilinestr,fpilines,wcoef,verbose=True):
     """ 
     Refit wavelength solution using FPI wavelengths
     fpilinestr: information on each unique FPI line
     fpilines: information on FPI full-frame
-    wavecal: original wavelength solution used
+    wcoef: original arclamp wavelength solution coefficients
     """
+
+    # Prune out lines that had unsuccessful fits or no mean wavelength
+    bd, = np.where((fpilines['success']==False) | (fpilines['lineid']==-1))
+    if len(bd)>0:
+        if verbose:
+            print('Pruning ',len(bd),' FPI lines with unsuccessful Gaussian fits or no mean line wavelength')
+        fpilines.remove_rows(bd)
 
     npoly = 4
     # increasing npoly doesn't improve the solution
@@ -311,8 +353,12 @@ def fpiwavesol(fpilinestr,fpilines,wavecal,verbose=True):
         ind, = np.where(fpilines['chip']==chip)
         chipnum[ind] = ichip
 
-    allpars = np.zeros((npoly,300),float)
+    allpars = np.zeros((npoly+3,300),float)
     allrms = np.zeros(300,float)
+
+    norder = 4
+    newwaves = np.zeros((3,300,2048),float)
+    newwcoef = np.zeros((3,norder+1,300),float)
 
     for row in np.arange(300):
         # set up independent variable array with pixel, chip, groupid, and dependent variable (wavelength)
@@ -320,6 +366,8 @@ def fpiwavesol(fpilinestr,fpilines,wavecal,verbose=True):
         if len(thisrow)==0:
             print('No lines for row ',row)
             continue
+
+        # Global 3-chip fitting
         x = np.zeros([3,len(thisrow)])
         x[0,:] = fpilines['pars'][thisrow,1]
         x[1,:] = chipnum[thisrow]+1
@@ -336,25 +384,23 @@ def fpiwavesol(fpilinestr,fpilines,wavecal,verbose=True):
         bounds[0][npoly+1] = -1.e-7
         bounds[1][npoly+1] = 1.e-7
 
-        # reject lines with bad wavelengths
-        gd, = np.where(y < 2e5)
-
         # Get initial guess from arclamp wavelength solution
         pars0 = np.zeros(npoly+3,float)
-        pars0[len(pars0)-7:] = wavecal['a'][3].data[:,row]
+        #pars0[len(pars0)-7:] = wavecal['a'][3].data[:,row]
+        pars0[len(pars0)-7:] = wcoef[:,row]
         
-
         # use curve_fit to optimize parameters
         try :
             if verbose:
-                print('row: ', row, 'nlines: ', len(thisrow), 'gd: ', len(gd))
+                print('row: ', row, 'nlines: ', len(thisrow))
                 print(pars0)
-            popt,pcov = curve_fit(wave.func_multi_poly,x[:,gd],y[gd],p0=pars0,bounds=bounds)
+            popt,pcov = curve_fit(wave.func_multi_poly,x,y,p0=pars0,bounds=bounds)
             pars = copy.copy(popt)
-            res = y-wave.func_multi_poly(x,*pars)
-            rms = np.sqrt(np.mean(res[gd]**2))
+            yfit = wave.func_multi_poly(x,*pars)
+            res = y-yfit
+            rms = np.sqrt(np.mean(res**2))
             if verbose:
-                print('res: ',len(gd),np.median(res),np.median(np.abs(res)),res[gd].std())
+                print('res: ',len(thisrow),np.median(res),np.median(np.abs(res)),dln.mad(np.array(res)))
                 print(pars)
         except :
             print('Solution failed for row: ', row)
@@ -363,39 +409,82 @@ def fpiwavesol(fpilinestr,fpilines,wavecal,verbose=True):
             rms = 999999.
 
         # Save the parameters
-        allpars[0:npoly,row] = popt[0:npoly]
+        allpars[:,row] = popt
         allrms[row] = rms
 
-        xglobal = np.zeros(len(thisrow),float)
-        g1,=np.where(x[1,:]==1)
-        g2,=np.where(x[1,:]==2)
-        g3,=np.where(x[1,:]==3)
-        xglobal[g1] = x[0,g1]-1023.5+pars[npoly]
-        xglobal[g2] = x[0,g2]-1023.5+pars[npoly+1]
-        xglobal[g3] = x[0,g3]-1023.5+pars[npoly+2]
+
+        # Do cubic fit to each chip separately
+        #  This produces better results
+        chres = np.zeros(len(thisrow),float)
+        for ichip,chip in enumerate(chips):
+            ind1, = np.where(fpilines['chip'][thisrow]==chip)
+            chind = thisrow[ind1]
+            xx = fpilines['pars'][chind,1]
+            yy = fpilines['linewave'][chind]
+            coef = robust.polyfit(xx,yy,4)  #3
+            yfit = robust.npp_polyval(xx,np.flip(coef))
+            chres1 = yy-yfit
+            chrms = np.sqrt(np.mean(chres1**2))
+            chres[ind1] = chres1
+            newwcoef[ichip,:,row] = coef
+            newwaves[ichip,row,:] = robust.npp_polyval(np.arange(2048),np.flip(coef))
+        sig = dln.mad(np.array(chres))
+        # this gets rid of the extra wiggle structure in the residuals
+        print('chip res: ',len(thisrow),np.median(chres),np.median(np.abs(chres)),sig)
+        
 
         # there is obvious structure in the residuals
         # fitting all lines from just ONE chip at a time gives much lower rms
         # -should I be fitting with local X values and higher order?
         # -should the original arclamp wavelength solutions use a higher order
         #   cubic seems very low
-        
-        figfile = 'wave_resid_fiber'+str(row)+'.png'
-        matplotlib.use('Agg')
-        fig,ax = plt.subplots()
-        plt.scatter(xglobal[gd],y[gd]-wave.func_multi_poly(x[:,gd],*pars))
-        plt.ylim([-0.02,0.02])
-        plt.xlabel('Xglobal (pix)')
-        plt.ylabel('Residuals (A)')
-        plt.title('Residual to global fit - fiber='+str(row))
-        plt.savefig(figfile,bbox_inches='tight')
-        print('Saving figure to ',figfile)
+        if plt==True:
+            xglobal = np.zeros(len(thisrow),float)
+            g1,=np.where(x[1,:]==1)
+            g2,=np.where(x[1,:]==2)
+            g3,=np.where(x[1,:]==3)
+            xglobal[g1] = x[0,g1]-1023.5-2048+pars[npoly]
+            xglobal[g2] = x[0,g2]-1023.5+pars[npoly+1]
+            xglobal[g3] = x[0,g3]-1023.5+2048+pars[npoly+2]
 
-        import pdb; pdb.set_trace()
+            figfile = 'wave_resid_fiber'+str(row)+'.png'
+            matplotlib.use('Agg')
+            fig,ax = plt.subplots()
+            plt.scatter(xglobal,res)
+            plt.scatter(xglobal,chres)
+            plt.ylim([-0.02,0.02])
+            plt.xlabel('Xglobal (pix)')
+            plt.ylabel('Residuals (A)')
+            plt.title('Residual to global fit - fiber='+str(row)+' rms='+str(rms)+' npoly='+str(npoly))
+            plt.savefig(figfile,bbox_inches='tight')
+            print('Saving figure to ',figfile)
 
-        #waves[chip][row,:] = func_multi_poly(x,*allpars[:,row],npoly=4)
+    #import pdb; pdb.set_trace()
 
-    import pdb; pdb.set_trace()
+    return newwcoef,newwaves
+    
+def save_fpiwave(outfile,mjd5,fpinum,fpiwcoef,fpiwaves,fpilinestr):
+    """
+    Save the FPI wavelength information
+    outfile: output file name
+    mjd5: day MJD value
+    fpinum: FPI full-frame exposure number
+    fpiwcoef: new wavelength coefficients [3,5,300]
+    fpiwaves: new wavelength array [3,300,2048]
+    fpilinestr: table of unique FPI lines and wavelengths
+    """
+
+    # Save the new wavelength solution
+    hdu = fits.HDUList()
+    hdu.append(fits.PrimaryHDU())
+    hdu[0].header['FRAME']=fpinum
+    hdu[0].header['COMMENT']='HDU#1 : wavelength calibration array [3,300,2048]'
+    hdu[0].header['COMMENT']='HDU#2 : wavelength calibration parameters [3,5,300]'
+    hdu[0].header['COMMENT']='HDU#3 : table of unique FPI lines and wavelengths'
+    hdu.append(fits.ImageHDU(fpiwaves))
+    hdu.append(fits.ImageHDU(fpiwcoef))
+    hdu.append(fits.table_to_hdu(Table(fpilinestr)))
+    hdu.writeto(outfile,overwrite=True)
 
 def frame2mjd(frame) :
     """ Get MJD from frame number """
