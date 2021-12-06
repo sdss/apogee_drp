b1b6c286ea1f51330dfe065054ab2082ab800a8a
drewchojnowski <drewski@nmsu.edu>
2021-12-06 12:17:29 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -527,7 +527,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
     axminlen = 3.5
     alf = 0.6
     markersz = 7
-    colors = np.array(['crimson', 'limegreen', 'orange', 'violet', 'royalblue'])
+    colors = np.array(['gold', 'orange', 'lightcoral', 'crimson', 'maroon'])
     colors1 = np.array(['k', 'salmon', 'limegreen', 'cornflowerblue'])
     colors2 = np.array(['dodgerblue', 'seagreen', 'orange'])
     fibers = np.array([10, 80, 150, 220, 290])[::-1]
