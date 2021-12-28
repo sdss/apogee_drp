1d49168d09801cdde90ddce14ce4903e59c916fd
David Nidever <dnidever@montana.edu>
2021-12-27 19:00:57 -0700

    updates to apogeedrp.py

--- python/apogee_drp/plan/apogeedrp.py
+++ python/apogee_drp/plan/apogeedrp.py
@@ -520,18 +520,24 @@ def run(observatory,mjdstart,mjdstop,apred,qos='sdss',fresh=False,links=None):
     calind, = np.where((expinfo['exptype']=='DOMEFLAT') | (expinfo['exptype']=='QUARTZFLAT') | 
                        (expinfo['exptype']=='ARCLAMP') | (expinfo['exptype']=='FPI'))
     if len(calind)>0:
-        calcodedict = {'DOMEFLAT':0, 'QUARTZFLAT':0, 'ARCLAMP':1, 'FPI':2}
-        calcode = [calcodedict[etype] for etype in expinfo['exptype'][calind]]
-        calnames = ['DOMEFLAT/QUARTZFLAT','ARCLAMP','FPI']
-        shcalnames = ['psf','arcs','fpi']
+        # psf, flux, arcs, fpi
+        calcodedict = [{'DOMEFLAT':True, 'QUARTZFLAT':True, 'ARCLAMP':False, 'FPI':False},
+                       {'DOMEFLAT':True, 'QUARTZFLAT':False, 'ARCLAMP':False, 'FPI':False},
+                       {'DOMEFLAT':False, 'QUARTZFLAT':False, 'ARCLAMP':True, 'FPI':False},
+                       {'DOMEFLAT':False, 'QUARTZFLAT':False, 'ARCLAMP':False, 'FPI':True}]
+        #calcodedict = {'DOMEFLAT':0, 'QUARTZFLAT':0, 'ARCLAMP':2, 'FPI':3}
+        #calcode = [calcodedict[etype] for etype in expinfo['exptype'][calind]]
+        calnames = ['DOMEFLAT/QUARTZFLAT','FLUX','ARCLAMP','FPI']
+        shcalnames = ['psf','flux','arcs','fpi']
         chkcal = []
-        for j,ccode in enumerate([0,1,2]):
+        for j,ccode in enumerate([0,1,2,3]):
             rootLogger.info('')
             rootLogger.info('----------------------------------------')
             rootLogger.info('Running Calibration Files: '+str(calnames[j]))
             rootLogger.info('========================================')
             rootLogger.info('')
-            cind, = np.where(np.array(calcode)==ccode)
+            calcode = [calcodedict[ccode][etype] for etype in expinfo['exptype'][calind]]            
+            cind, = np.where(np.array(calcode)==True)
             if len(cind)>0:
                 rootLogger.info(str(len(cind))+' files to run')
                 queue = pbsqueue(verbose=True)
@@ -543,15 +549,19 @@ def run(observatory,mjdstart,mjdstop,apred,qos='sdss',fresh=False,links=None):
                     num1 = expinfo['num'][calind[cind[k]]]
                     exptype1 = expinfo['exptype'][calind[cind[k]]]
                     rootLogger.info('Calibration file %d : %s %d' % (k+1,exptype1,num1))
-                    if exptype1=='DOMEFLAT' or exptype1=='QUARTZFLAT':
+                    if ccode==0:   # psfs
                         cmd1 = 'makecal --psf '+str(num1)+' --unlock'
                         if clobber: cmd1 += ' --clobber'
                         logfile1 = calplandir+'/apPSF-'+str(num1)+'_pbs.'+logtime+'.log'
-                    if exptype1=='ARCLAMP':
+                    elif ccode==1   # flux
+                        cmd1 = 'makecal --psf '+str(num1)+' --flux '+str(num1)+' --unlock'
+                        if clobber: cmd1 += ' --clobber'
+                        logfile1 = calplandir+'/apFlux-'+str(num1)+'_pbs.'+logtime+'.log'
+                    elif ccode==2:  # arcs
                         cmd1 = 'makecal --wave '+str(num1)+' --unlock'
                         if clobber: cmd1 += ' --clobber'
                         logfile1 = calplandir+'/apWave-'+str(num1)+'_pbs.'+logtime+'.log'
-                    if exptype1=='FPI':
+                    elif ccode==3:  # fpi
                         cmd1 = 'makecal --fpi '+str(num1)+' --unlock'
                         if clobber: cmd1 += ' --clobber'
                         logfile1 = calplandir+'/apFPI-'+str(num1)+'_pbs.'+logtime+'.log'
