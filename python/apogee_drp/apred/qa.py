4d07dafd1a5a61c0312edfcbbd627c56cccad6af
drewchojnowski <drewski@nmsu.edu>
2022-01-13 13:47:05 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -3359,11 +3359,8 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
             p, = np.where((iprogram == 'RM') | (iprogram == 'RMv2'))
             if len(p) > 0: ax1.scatter(x[p], y[p], marker='o', s=msz, edgecolors='k', alpha=alf, c='#B3E5FC', label='RM ('+str(len(p))+')')
 
-            p, = np.where(iprogram == 'AQMES-Wide')
+            p, = np.where((iprogram == 'AQMES-Wide') | (iprogram == 'AQMES-Medium') | (iprogram == 'AQMES-Bonus'):
             if len(p) > 0: ax1.scatter(x[p], y[p], marker='^', s=msz, edgecolors='k', alpha=alf, c='#DCEDC8', label='AQMES-Wide ('+str(len(p))+')')
-
-            p, = np.where(iprogram == 'AQMES-Medium')
-            if len(p) > 0: ax1.scatter(x[p], y[p], marker='v', s=msz, edgecolors='k', alpha=alf, c='#AED581', label='AQMES-Medium ('+str(len(p))+')')
 
             p, = np.where((iprogram == 'MWM') | (iprogram == 'MWM_30min') | (iprogram == 'halo_dsph') | (iprogram == 'MWM2') | (iprogram == 'MWM2_sky')
                                               | (iprogram == 'MWM3') | (iprogram == 'MWM_30min2') | (iprogram == 'MWM_30min3'))
@@ -3372,8 +3369,8 @@ def makeMasterQApages(mjdmin=None, mjdmax=None, apred=None, mjdfilebase=None, fi
             p, = np.where((iprogram == 'eFEDS1') | (iprogram == 'eFEDS2') | (iprogram == 'eFEDS3'))
             if len(p) > 0: ax1.scatter(x[p], y[p], marker='s', s=msz, edgecolors='k', alpha=alf, c='#FFF9C4', label='eFEDS ('+str(len(p))+')')
 
-            p, = np.where((iprogram != 'RM') & (iprogram != 'RMv2') & (iprogram != 'AQMES-Wide') & (iprogram != 'AQMES-Medium') & (iprogram != 'MWM') & 
-                          (iprogram != 'eFEDS1') & (iprogram != 'eFEDS2') & (iprogram != 'eFEDS3'))
+            p, = np.where((iprogram != 'RM') & (iprogram != 'RMv2') & (iprogram != 'AQMES-Wide') & (iprogram != 'AQMES-Medium') & (iprogram != 'AQMES-Bonus') & 
+                          (iprogram != 'MWM') & (iprogram != 'eFEDS1') & (iprogram != 'eFEDS2') & (iprogram != 'eFEDS3'))
             if len(p) > 0: ax1.scatter(x[p], y[p], marker='P', s=msz, edgecolors='k', alpha=alf, c='#ffb3b3', label='other ('+str(len(p))+')')
 
             ax1.text(0.5,1.04,ptype.capitalize(),transform=ax1.transAxes,ha='center')
