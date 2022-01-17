1fdf2b7370405005130789a794b7c65c4354aacf
David Nidever <dnidever@montana.edu>
2022-01-17 00:27:22 -0700

    updates to check.py

--- python/apogee_drp/plan/check.py
+++ python/apogee_drp/plan/check.py
@@ -13,11 +13,10 @@ except ImportError:
 
 from dlnpyutils import utils as dln
 from apogee_drp.utils import spectra,yanny,apload,platedata,utils
-from apogee_drp.plan import mkslurm
-from apogee_drp.apred import mkcal
 from sdss_access.path import path
 from astropy.io import fits
 from collections import OrderedDict
+from scipy.signal import medfilt2d
 
 # This checks APOGEE exposures to make sure that they are okay
 
@@ -32,8 +31,8 @@ def getinfo(num,apred,telescope):
     """ Get the info needed to check various things."""
 
     info = {'num':int(num), 'mjd':0, 'filename3d':'', 'exists3d':False,
-            'exptype':'', 'nread':-1, 'gangstat':None,'shutter':None,
-            'filename2d':'', 'exists2d':False}
+            'exptype':'', 'nread':-1, 'gangstat':None, 'shutter':None,
+            'calshutter':'', 'filename2d':'', 'exists2d':False}
 
     load = apload.ApLoad(apred=apred,telescope=telescope)
     mjd = int(load.cmjd(num))
@@ -56,8 +55,10 @@ def getinfo(num,apred,telescope):
     info['nread'] = nread
     # Gang state
     info['gangstat'] = head.get('gangstat')
-    # Shutter state
+    # APOGEE Shutter state
     info['shutter'] = head.get('shutter')
+    # CalBox shutter status
+    info['calshutter'] = head.get('lampshtr')
     # Does 2D file exist
     mjd = int(load.cmjd(num))
     filename2d = load.filename('2D',num=num,mjd=mjd,chips=True).replace('2D-','2D-b-')
@@ -140,9 +141,14 @@ def check_object(num,apred,telescope):
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        # There's a bright sky line around X=1117
+        sub = im[:,1117-100:1117+100]
+        smsub = medfilt2d(sub,(1,7))  # smooth in spectral axis
+        resmsub = dln.rebin(smsub,(2048//8,200),tot=True) # rebin in spatial axis
+        peakflux = np.nanmax(resmsub,axis=1)  # peak flux feature in spectral dim.
+        avgpeakflux = np.nanmean(peakflux)
         # Check skyline flux
-        if med/info['nread']<0.1:
+        if avgpeakflux/info['nread']<200:
             mask |= 2**4
         #print('object',med/info['nread'])
 
@@ -181,9 +187,11 @@ def check_domeflat(num,apred,telescope):
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        medim = np.nanmedian(im[:,900:1100],axis=1)
+        remedim = dln.rebin(medim,2048//8,tot=True) # rebin in spatial axis
+        avgpeakflux = np.nanmean(remedim)
         # Check the flux
-        if med/info['nread']<20:
+        if avgpeakflux/info['nread']<500:
             mask |= 2**4
         #print('domeflat',med/info['nread'])
 
@@ -214,17 +222,24 @@ def check_quartzflat(num,apred,telescope):
     if info['gangstat'] is not None:
         if info['gangstat']!='Podium':
             mask |= 2**2
-    # 3 - Wrong shutter state
+    # 3 - Wrong APOGEE shutter state
     if info['shutter'] is not None:
         # shutter must be open for quartzflat exposures
         if info['shutter']=='Closed':
             mask |= 2**3
+    # cal shutter state
+    if info['calshutter'] is not None:
+        # shutter must be open for quartzflat exposures
+        if info['calshutter']==False:
+            mask |= 2**3            
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        medim = np.nanmedian(im[:,900:1100],axis=1)
+        remedim = dln.rebin(medim,2048//8,tot=True) # rebin in spatial axis
+        avgpeakflux = np.nanmean(remedim)
         # Check the flux
-        if med/info['nread']<50:
+        if avgpeakflux/info['nread']<500:
             mask |= 2**4
         #print('quartzflat',med/info['nread'])
 
@@ -260,16 +275,39 @@ def check_arclamp(num,apred,telescope):
         # shutter must be open for arclamp exposures
         if info['shutter']=='Closed':
             mask |= 2**3
+    # cal shutter state
+    if info['calshutter'] is not None:
+        # shutter must be open for arclamp exposures
+        if info['calshutter']==False:
+            mask |= 2**3            
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        head = fits.getheader(info['filename2d'])
+        # UNE
+        #  bright line at X=1452
+        if head.get('LAMPUNE')==True:
+            sub = im[:,1452-100:1452+100]
+            thresh = 40
+        # THARNE
+        #  bright line at X=1566
+        elif head.get('LAMPTHAR')==True:
+            sub = im[:,1566-100:1566+100]
+            thresh = 1000
+        else:
+            sub = im[:,900:1100]
+            thresh = 10
+        smsub = medfilt2d(sub,(1,7))  # smooth in spectral axis
+        resmsub = dln.rebin(smsub,(2048//8,200),tot=True) # rebin in spatial axis
+        peakflux = np.nanmax(resmsub,axis=1)  # peak flux feature in spectral dim.
+        avgpeakflux = np.nanmean(peakflux)
         # Check the line flux
-        if med/info['nread']<0.1:
+        if avgpeakflux/info['nread']<thresh:
             mask |= 2**4
         #print('arclamp',med/info['nread'])
 
     return mask
+
 
 def check_fpi(num,apred,telescope):
     """ check FPI exposure."""
@@ -300,12 +338,21 @@ def check_fpi(num,apred,telescope):
         # shutter must be open for FPI exposures
         if info['shutter']=='Closed':
             mask |= 2**3
+    # cal shutter state
+    if info['calshutter'] is not None:
+        # shutter must be open for fpi exposures
+        if info['calshutter']==False:
+            mask |= 2**3            
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        sub = im[:,900:1100]
+        smsub = medfilt2d(sub,(1,7))  # smooth in spectral axis
+        resmsub = dln.rebin(smsub,(2048//8,200),tot=True) # rebin in spatial axis
+        peakflux = np.nanmax(resmsub,axis=1)  # peak flux feature in spectral dim.
+        avgpeakflux = np.nanmean(peakflux)
         # Check the flux
-        if med/info['nread']<5:
+        if avgpeakflux/info['nread']<70:
             mask |= 2**4
 
     return mask
@@ -343,9 +390,9 @@ def check_internalflat(num,apred,telescope):
     # 4 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
+        med = np.nanmedian(im)
         # Check the flux
-        if med/info['nread']<50:
+        if med/info['nread']<300:
             mask |= 2**4
         #print('internalflat',med/info['nread'])
 
@@ -384,9 +431,14 @@ def check_skyflat(num,apred,telescope):
     # 5 - Wrong flux
     if info['exists2d']==True:
         im = fits.getdata(info['filename2d'],1)
-        med = np.median(im[:,900:1100])
-        # Check the flux
-        if med/info['nread']<0.1:
+        # There's a bright sky line around X=1117
+        sub = im[:,1117-100:1117+100]
+        smsub = medfilt2d(sub,(1,7))  # smooth in spectral axis
+        resmsub = dln.rebin(smsub,(2048//8,200),tot=True) # rebin in spatial axis
+        peakflux = np.nanmax(resmsub,axis=1)  # peak flux feature in spectral dim.
+        avgpeakflux = np.nanmean(peakflux)
+        # Check skyline flux
+        if avgpeakflux/info['nread']<200:
             mask |= 2**4
 
     return mask
