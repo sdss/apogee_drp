9c514cc3a4a527e5824a517fc1dce745bc3d1efa
David Nidever <dnidever@montana.edu>
2022-01-26 09:12:05 -0700

    updated runapogee, rv and qa code to deal with another missing apogee_id case

--- python/apogee_drp/plan/runapogee.py
+++ python/apogee_drp/plan/runapogee.py
@@ -1049,7 +1049,7 @@ def run_daily(observatory,mjd5=None,apred=None,qos='sdss-fast',clobber=False):
         objects,ui = np.unique(vcat['apogee_id'],return_index=True)
         vcat = vcat[ui]
         # remove ones with missing or blank apogee_ids
-        bd, = np.where((vcat['apogee_id']=='') | (vcat['apogee_id']=='None') | (vcat['apogee_id']=='2MNone'))
+        bd, = np.where((vcat['apogee_id']=='') | (vcat['apogee_id']=='None') | (vcat['apogee_id']=='2MNone') | (vcat['apogee_id']=='2M'))
         if len(bd)>0:
             vcat = np.delete(vcat,bd)
         for obj in vcat['apogee_id']:
