0ce144a24a9bb02a0747a00d9d3df34825c94021
drewchojnowski <drewski@nmsu.edu>
2022-01-28 11:58:15 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -781,7 +781,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                     if ichip == 2: chp = 'a'
                     file1d = load.filename('1D', mjd=str(umjd[idome]), num=gdcal['NUM'][idome], chips='c')
                     file1d = file1d.replace('1D-', '1D-' + chp + '-')
-                    #pdb.set_trace
+                    pdb.set_trace
                     if os.path.exists(file1d):
                         oned = fits.getdata(file1d)
                         onedflux = np.nanmedian(oned, axis=1)[::-1]
