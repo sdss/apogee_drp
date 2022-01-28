4a0836efd1ed4b23bd74902d35869fb5c923a07d
David Nidever <dnidever@montana.edu>
2022-01-28 14:02:04 -0700

    ap3d.py updates

--- python/apogee_drp/utils/utils.py
+++ python/apogee_drp/utils/utils.py
@@ -11,3 +11,18 @@ def getmjd5(dateobs):
     # Truncate for MJD5 number                                                                                                                                
     mjd5 = int(mjd)
     return mjd5
+
+def writelog(logfile,line):
+    """ Append lines to a logfile."""
+    # Convert to list
+    if type(line) is list:
+        lines = line
+    elif type(line) is str:
+        lines = [line]
+    else:
+        lines = [str(line)]
+    # Make sure each line ends in newline
+    lines = [l+'\n' if l.endswith('\n')==False else l for l in lines]
+    # Append to the file
+    with open(logfile,'a') as f:
+        f.writelines(lines)
