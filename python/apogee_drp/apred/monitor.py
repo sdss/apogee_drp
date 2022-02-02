833330d78b21762b805d90df2bb49955ca6cdbe8
drewchojnowski <drewski@nmsu.edu>
2022-02-02 13:39:22 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -813,7 +813,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
             plt.savefig(plotfile)
             plt.close('all')
 
-        return
+        #return
 
         ###########################################################################################
         # dillum1.png
