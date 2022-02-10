4b70a6653ff2b240ed674feafe7e660baedf039b
drewchojnowski <drewski@nmsu.edu>
2022-02-10 09:23:23 -0700

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1790,7 +1790,7 @@ def makeVisHTML(load=None, plate=None, mjd=None, survey=None, apred=None, telesc
 
     # DB query for this visit
     db = apogeedb.DBSession()
-    vcat = db.query('visit_latest', where="plate='" + plate + "' and mjd='" + mjd + "'", fmt='table')
+    vcat = db.query('visit', where="plate='" + plate + "' and mjd='" + mjd + "'", fmt='table')
     pdb.set_trace()
     # Loop over the fibers
     for j in range(300):
