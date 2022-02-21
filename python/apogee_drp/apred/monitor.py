5c6f9199b301843638cf5a602a512ade635ee7c4
drewchojnowski <drewski@nmsu.edu>
2022-02-21 13:18:07 -0700

    updating monitor.py

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -900,7 +900,8 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                 #ax.plot([xx,xx], [minh[i],maxh[i]], color='k', zorder=1)
 
             g, = np.where(pl < 10000)
-            ax.scatter(pl[g], meanh[g], marker='o', s=50, c='cyan', edgecolors='k')#, c=colors[ifib], alpha=alf)#, label='Fiber ' + str(fibers[ifib]))
+            upl,uind = np.unique(meanh[g], return_index=True)
+            ax.scatter(upl, meanh[g][uind], marker='o', s=50, c='cyan', edgecolors='k')#, c=colors[ifib], alpha=alf)#, label='Fiber ' + str(fibers[ifib]))
             g, = np.where(pl > 10000)
             ax.scatter(pl[g]-10000, meanh[g], marker='o', s=50, c='red', edgecolors='k')#, c=colors[ifib], alpha=alf)#, label='Fiber ' + str(fibers[ifib]))
 
