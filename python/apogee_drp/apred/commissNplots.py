01f3d55ee9aba99cea49eba74753e80581744db8
drewchojnowski <drewski@nmsu.edu>
2022-02-22 13:24:03 -0700

    updating commissNplots.py

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -243,7 +243,7 @@ def rvparams(allv4=None, allv5=None, remake=False, restrict=False):
     symsz = 40
     cmap = 'rainbow'
     vmin = 0.2
-    vmax = 1.2
+    vmax = 1.0
 
     g, = np.where((np.isnan(gdata['TEFF'][:,0]) == False) & (np.isnan(gdata['TEFF'][:,1]) == False) & (gdata['TEFF'][:,0] < 7500))
     x = gdata['VHELIO'][:,0][g]
