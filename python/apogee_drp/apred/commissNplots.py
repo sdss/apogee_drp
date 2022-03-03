cf8c6c057bacdc362d9c12cf599e7af8d8089832
drewchojnowski <drewski@nmsu.edu>
2022-03-03 13:46:38 -0700

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -558,7 +558,7 @@ def dillum2(mjdstart=59557, mjdmean=False, chip=2, do59557=False):
     ax4.grid(True)
 
     mad = dln.mad(percentDif)
-    txt = 'MAD = ' + str("%.3f" % round(madresid, 3)) + '%'
+    txt = 'MAD = ' + str("%.3f" % round(mad, 3)) + '%'
     ax4.text(0.5, 0.95, txt, transform=ax3.transAxes, ha='center', va='top', bbox=bboxpar)
 
     fig.subplots_adjust(left=0.084,right=0.985,bottom=0.043,top=0.95,hspace=0.08,wspace=0.1)
