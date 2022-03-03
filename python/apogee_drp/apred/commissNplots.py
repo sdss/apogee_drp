494e588a62e7ef827ae68c0f832acbdc734d8728
drewchojnowski <drewski@nmsu.edu>
2022-03-03 13:43:12 -0700

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -496,7 +496,7 @@ def dillum2(mjdstart=59557, mjdmean=False, chip=2, do59557=False):
     i = 0
     for ax in axes:
         ax.set_xlim(0, 301)
-        ax.minorticks_on()
+        if i != 3: ax.minorticks_on()
         ax.xaxis.set_major_locator(ticker.MultipleLocator(20))
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
         ax.tick_params(axis='both',which='both',direction='in',bottom=True,top=True,left=True,right=True)
