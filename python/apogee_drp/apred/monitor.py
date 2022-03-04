2855b4bedc5534dca3842752a354d97740508124
drewchojnowski <drewski@nmsu.edu>
2022-03-04 13:06:17 -0700

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -1062,9 +1062,9 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                 chip = chips[ichip]
 
                 ax = plt.subplot2grid((nchips,1), (ichip,0))
-                ax.set_xlim(xmin, xmax)
+                ax.set_xlim(58500, xmax)
                 ax.set_ylim(ymin, ymax)
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(500))
+                #ax.xaxis.set_major_locator(ticker.MultipleLocator(500))
                 ax.minorticks_on()
                 ax.tick_params(axis='both',which='both',direction='in',bottom=True,top=True,left=True,right=True)
                 ax.tick_params(axis='both',which='major',length=axmajlen)
