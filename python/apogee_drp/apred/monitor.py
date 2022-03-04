36eeccc82c0d10283b2770428765edc621fc743a
drewchojnowski <drewski@nmsu.edu>
2022-03-04 12:47:13 -0700

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -1038,23 +1038,25 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
 
         ###########################################################################################
         # qtrace.png
+        # Time series plot of median dome flat flux from cross sections across fibers
         plotfile = specdir5 + 'monitor/' + instrument + '/qtrace.png'
         if (os.path.exists(plotfile) == False) | (clobber == True):
             print("----> monitor: Making " + os.path.basename(plotfile))
 
+            gfibers = np.array([10, 45, 80, 115, 150, 185, 220, 255, 290])[::-1]
+            gcolors = np.array(['midnightblue', 'deepskyblue', 'mediumorchid', 'red', 'orange', 'magenta', 'darkgreen', 'limegreen', 'maroon'])[::-1]
+            #gfibers = np.array([0, 49, 99, 149, 199, 249, 299])[::-1]
+            ngplotfibs = len(gfibers)
+
             fig = plt.figure(figsize=(30,14))
-            ymax = 1013
-            ymin = 1039
+            ymax = 2.8
+            ymin = -2.8
             yspan = ymax - ymin
-
-            fibs = np.arange(148,153)
-            nfibs = len(fibs)
-
-            gd, = np.where(qtrace['MJD'] > 50000)
-            qtz = qtrace[gd]
-            qpos = qtz['GAUSS_CENT']
-            eqpos = qtz['E_GAUSS_CENT']
-            qmjd = qtz['MJD']
+            dtrace = fits.getdata(specdir5 + 'monitor/' + instrument + 'QuartzFlatTrace-all.fits')
+            gd, = np.where((dtrace['MJD'] > 1000) & (dtrace['GAUSS_NPEAKS'][:,1] > 280))
+            gdtrace = dtrace[gd]
+            gcent = gdtrace['GAUSS_CENT'][:,:,gfibers]
+            xvals = gdtrace['MJD']
 
             for ichip in range(nchips):
                 chip = chips[ichip]
@@ -1069,31 +1071,31 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                 ax.tick_params(axis='both',which='minor',length=axminlen)
                 ax.tick_params(axis='both',which='both',width=axwidth)
                 if ichip == nchips-1: ax.set_xlabel(r'JD - 2,400,000')
-                ax.set_ylabel(r'Trace Position (pix)')
+                if ichip == 1: ax.set_ylabel(r'Quartz Lamp Trace Position Residuals (pixels)')
                 if ichip < nchips-1: ax.axes.xaxis.set_ticklabels([])
-                ax.axvline(x=59146, color='teal', linewidth=2)
-                ax.axvline(x=59555, color='teal', linewidth=2)
-                ax.text(59146-xspan*0.005, ymax-yspan*0.04, 'plate-III+IV', fontsize=fsz, color='teal', va='top', ha='right', bbox=bboxpar)
-                ax.text(59353, ymax-yspan*0.04, 'plate-V', fontsize=fsz, color='teal', va='top', ha='center', bbox=bboxpar)
-                ax.text(59555+xspan*0.005, ymax-yspan*0.04, 'FPS-V', fontsize=fsz, color='teal', va='top', ha='left', bbox=bboxpar)
-                ax.text(0.01,0.96,chip.capitalize() + '\n' + 'Chip', transform=ax.transAxes, fontsize=fsz, ha='left', va='top', color=chip, bbox=bboxpar)
+                ax.axvline(x=59146, color='r', linewidth=2)
+                ax.axhline(y=0, color='k', linestyle='dashed', alpha=alf)
 
                 for iyear in range(nyears):
                     ax.axvline(x=yearjd[iyear], color='k', linestyle='dashed', alpha=alf)
                     if ichip == 0: ax.text(yearjd[iyear], ymax+yspan*0.025, cyears[iyear], ha='center')
 
-                for ifib in range(nfibs):
-                    yvals = qpos[:, ichip, fibs[ifib]]
-                    ax.scatter(qmjd, yvals, marker='o', s=markersz, c=colors[ifib], alpha=alf, 
-                               label='fib ' + str(fibs[ifib]))
+                for ifib in range(ngplotfibs):
+                    medcent = np.nanmedian(gcent[:, ichip, ifib])
+                    yvals = gcent[:, ichip, ifib] - medcent
+                    ax.scatter(xvals, yvals, marker='o', s=markersz, c=gcolors[ifib], alpha=alf, 
+                               label='fib ' + str(gfibers[ifib]))
 
+                ax.text(0.97,0.92,chip.capitalize() + '\n' + 'Chip', transform=ax.transAxes, 
+                        ha='center', va='top', color=chip, bbox=bboxpar)
                 if ichip == 0: 
                     ax.legend(loc='lower right', labelspacing=0.5, handletextpad=-0.1, markerscale=4, 
-                              fontsize=fsz, edgecolor='k', framealpha=1)
+                              fontsize=fsz*0.8, edgecolor='k', framealpha=1)
 
-            fig.subplots_adjust(left=0.06,right=0.995,bottom=0.06,top=0.96,hspace=0.08,wspace=0.00)
+            fig.subplots_adjust(left=0.04,right=0.995,bottom=0.06,top=0.96,hspace=0.08,wspace=0.00)
             plt.savefig(plotfile)
             plt.close('all')
+
 
         ###########################################################################################
         # qfwhm.png
@@ -3564,65 +3566,7 @@ def getSnrStruct2(data1=None, data2=None, iexp=None, field=None, sumfile=None):
 
         return
 
-        ###########################################################################################
-        # qtrace.png
-        # Time series plot of median dome flat flux from cross sections across fibers
-        plotfile = specdir5 + 'monitor/' + instrument + '/qtrace.png'
-        if (os.path.exists(plotfile) == False) | (clobber == True):
-            print("----> monitor: Making " + os.path.basename(plotfile))
 
-            gfibers = np.array([10, 45, 80, 115, 150, 185, 220, 255, 290])[::-1]
-            gcolors = np.array(['midnightblue', 'deepskyblue', 'mediumorchid', 'red', 'orange', 'magenta', 'darkgreen', 'limegreen', 'maroon'])[::-1]
-            #gfibers = np.array([0, 49, 99, 149, 199, 249, 299])[::-1]
-            ngplotfibs = len(gfibers)
-
-            fig = plt.figure(figsize=(30,14))
-            ymax = 2.8
-            ymin = -2.8
-            yspan = ymax - ymin
-            dtrace = fits.getdata(specdir5 + 'monitor/' + instrument + 'QuartzFlatTrace-all.fits')
-            gd, = np.where((dtrace['MJD'] > 1000) & (dtrace['GAUSS_NPEAKS'][:,1] > 280))
-            gdtrace = dtrace[gd]
-            gcent = gdtrace['GAUSS_CENT'][:,:,gfibers]
-            xvals = gdtrace['MJD']
-
-            for ichip in range(nchips):
-                chip = chips[ichip]
-
-                ax = plt.subplot2grid((nchips,1), (ichip,0))
-                ax.set_xlim(xmin, xmax)
-                ax.set_ylim(ymin, ymax)
-                ax.xaxis.set_major_locator(ticker.MultipleLocator(500))
-                ax.minorticks_on()
-                ax.tick_params(axis='both',which='both',direction='in',bottom=True,top=True,left=True,right=True)
-                ax.tick_params(axis='both',which='major',length=axmajlen)
-                ax.tick_params(axis='both',which='minor',length=axminlen)
-                ax.tick_params(axis='both',which='both',width=axwidth)
-                if ichip == nchips-1: ax.set_xlabel(r'JD - 2,400,000')
-                if ichip == 1: ax.set_ylabel(r'Quartz Lamp Trace Position Residuals (pixels)')
-                if ichip < nchips-1: ax.axes.xaxis.set_ticklabels([])
-                ax.axvline(x=59146, color='r', linewidth=2)
-                ax.axhline(y=0, color='k', linestyle='dashed', alpha=alf)
-
-                for iyear in range(nyears):
-                    ax.axvline(x=yearjd[iyear], color='k', linestyle='dashed', alpha=alf)
-                    if ichip == 0: ax.text(yearjd[iyear], ymax+yspan*0.025, cyears[iyear], ha='center')
-
-                for ifib in range(ngplotfibs):
-                    medcent = np.nanmedian(gcent[:, ichip, ifib])
-                    yvals = gcent[:, ichip, ifib] - medcent
-                    ax.scatter(xvals, yvals, marker='o', s=markersz, c=gcolors[ifib], alpha=alf, 
-                               label='fib ' + str(gfibers[ifib]))
-
-                ax.text(0.97,0.92,chip.capitalize() + '\n' + 'Chip', transform=ax.transAxes, 
-                        ha='center', va='top', color=chip, bbox=bboxpar)
-                if ichip == 0: 
-                    ax.legend(loc='lower right', labelspacing=0.5, handletextpad=-0.1, markerscale=4, 
-                              fontsize=fsz*0.8, edgecolor='k', framealpha=1)
-
-            fig.subplots_adjust(left=0.04,right=0.995,bottom=0.06,top=0.96,hspace=0.08,wspace=0.00)
-            plt.savefig(plotfile)
-            plt.close('all')
 
         ###########################################################################################
         # dflattrace.png
