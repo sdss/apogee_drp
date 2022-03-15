6a93a1b368f0fc75313e075f0f7d7a0d77385ac9
drewchojnowski <drewski@nmsu.edu>
2022-03-15 10:32:48 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -168,7 +168,7 @@ def telescopePos(field='17049', star='2M07311564+3145469'):
             pmf = str(allsnrg['plate'][iexp])+'-'+str(allsnrg['mjd'][iexp])+'-'+str(obj['fiberid'][g][0]).zfill(3)
             alt = str("%.1f" % round(allexp['ALT'][g1][0],1))
             txt = alt + '   ' + pmf
-            txt = str(allsnrg['IM'][iexp]) + '  ' + str("%.1f" % round(allexp['ALT'][g1][0],1))
+            txt = str(allsnrg['IM'][iexp]) + '   ' + str("%.1f" % round(allexp['ALT'][g1][0],1))
             ax1.text(1.01, 0.97-.04*iexp, txt, color=c, fontsize=fsz, transform=ax1.transAxes, va='top')
             ax2.plot(wave[g][0], flux[g][0]/np.nanmedian(flux[g][0]), color=c)
 
