a6882bfa20a88a45bc7f359d3b4f63c1f7ba9084
drewchojnowski <drewski@nmsu.edu>
2022-03-15 10:27:41 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -143,7 +143,7 @@ def telescopePos(field='17049', star='2M07311564+3145469'):
     ax2.set_ylim(0.25, 1.35)
     visdir = specdir5 + 'visit/apo25m/' + field + '/'
 
-    ax1.text(1.01, 1.00, r'ALT($^{\circ}$) FIB MJD    S/N', transform=ax1.transAxes, fontsize=fsz)
+    ax1.text(1.01, 1.00, r'ALT($^{\circ}$) MJD-FIB      S/N', transform=ax1.transAxes, fontsize=fsz)
 
     ymx = np.zeros(nexp)
     for iexp in range(nexp):
@@ -165,7 +165,7 @@ def telescopePos(field='17049', star='2M07311564+3145469'):
             c = p[0].get_color()
             #txt = 'alt = ' + str("%.3f" % round(allexp['ALT'][g1][0],3)) + r'$^{\circ}$,  fiberID = ' + str(obj['fiberid'][g][0]).zfill(3)
             #txt1 = ',  mjd = ' + str(allsnrg['mjd'][iexp])
-            txt = str("%.1f" % round(allexp['ALT'][g1][0],1)) + '    ' + str(obj['fiberid'][g][0]).zfill(3) + '    ' + str(allsnrg['mjd'][iexp])
+            txt = str("%.1f" % round(allexp['ALT'][g1][0],1)) + '    ' + str(allsnrg['mjd'][iexp]) + '-' + str(obj['fiberid'][g][0]).zfill(3)
             ax1.text(1.01, 0.97-.04*iexp, txt, color=c, fontsize=fsz, transform=ax1.transAxes, va='top')
             ax2.plot(wave[g][0], flux[g][0]/np.nanmedian(flux[g][0]), color=c)
 
