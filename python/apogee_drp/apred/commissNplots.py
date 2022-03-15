da4c3f9a8410029d90e305debd92cfafadf28517
drewchojnowski <drewski@nmsu.edu>
2022-03-15 11:51:34 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -128,7 +128,7 @@ def telescopePos(field='17049', star='2M07311564+3145469', cmap='hot'):
     nexp = len(allsnrg)
 
     cmap = cmaps.get_cmap(cmap, 100)
-    cmapConst = 0.5
+    cmapConst = 0.1
     cmapShift = 0.01
 
     fig = plt.figure(figsize=(28,16))
