4b40f3de17c0253ee72645e6e9af20aa6b482f94
drewchojnowski <drewski@nmsu.edu>
2022-03-15 12:06:35 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -203,7 +203,8 @@ def telescopePos(field='17049', star='2M07311564+3145469', cmap='gnuplot2_r'):
             txt = star + r'  ($H=$' + str("%.3f" % round(obj['hmag'][g][0],3)) + ')'
             if iexp == 0: ax1.text(0.5, 1.02, txt, transform=ax1.transAxes, ha='center')
             #c = cmap(secz[iexp]/np.max(secz))
-            c = cmap(cmapShift+(secz[iexp]/np.max(secz))*cmapConst)
+            #c = cmap(cmapShift+(secz[iexp]/np.max(secz))*cmapConst)
+            c = cmap((iexp+1)/nexp)
             print(c)
             w = wave[g][0]; f = flux[g][0]
             p = ax1.plot(w, f, color=c)
