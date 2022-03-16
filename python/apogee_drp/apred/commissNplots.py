26c1b17397a749fd5bf7cbb16d86015efb28b150
drewchojnowski <drewski@nmsu.edu>
2022-03-16 09:47:06 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -322,7 +322,7 @@ def telescopePos2(field='17049', cmap='gnuplot_r'):
         obj = fits.getdata(cfile,11)
         gg1, = np.where(allsnrg['IM'][iexp] == pl1['IM'])
         if iexp == 0:
-            gdstars, = np.where((obj['objtype'] == 'STAR') & (obj['hmag'] < 9) & (pl2[:, gg1[0], 0] > 100))
+            gdstars, = np.where((obj['objtype'] == 'STAR') & (obj['hmag'] < 9) & (pl2['sn'][:, gg1[0], 0] > 100))
         w = np.nanmean(wave[gdstars], axis=0)       
         pdb.set_trace() 
         c = cmap(((iexp+1)/nexp)+cmapShift)
