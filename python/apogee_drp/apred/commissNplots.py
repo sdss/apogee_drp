9b63fd97c04823d9a1aab264163b0a91b0b5f3d5
drewchojnowski <drewski@nmsu.edu>
2022-03-16 12:22:55 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -353,6 +353,124 @@ def telescopePos2(field='17049', cmap='gnuplot_r'):
     return
 
 ###########################################################################################
+def telescopePos3(field='17049', star='2M07303923+3111106', cmap='gnuplot_r'):
+    # telescopePosPerform.png
+    plotfile = specdir5 + 'monitor/' + instrument + '/telescopePos/telescopePos_' + field + '_' + star + '.png'
+    print("----> commissNplots: Making " + os.path.basename(plotfile))
+
+    num = allsci['IM']
+    p, = np.where((num == 40630031) | (num == 40630039) | (num == 40630040) | 
+                  (num == 40630048) | (num == 40630049) | (num == 40630057) |
+                  (num == 40630058))
+    upl,uind = np.unique(allsci['plate'][p], return_index=True)
+    upl = allsci['plate'][p]#[uind]
+    umjd = allsci['mjd'][p]#[uind]
+    allscig = allsci[p]#[uind]
+    nexp = len(allscig)
+
+    cmap = cmaps.get_cmap(cmap, 100)
+    cmapConst = 0.5
+    cmapShift = 0.1
+
+    fig = plt.figure(figsize=(28,14))
+    ax1 = plt.subplot2grid((2,8), (0,0), colspan=7)
+    ax2 = plt.subplot2grid((2,8), (1,0), colspan=7)
+    ax11 = plt.subplot2grid((2,8), (0,7), colspan=1)
+    ax22 = plt.subplot2grid((2,8), (1,7), colspan=1)
+    #ax3 = plt.subplot2grid((2,8), (1,6), colspan=1)
+    axes = [ax1, ax2, ax11, ax22]
+    for ax in axes:
+        ax.minorticks_on()
+        ax.tick_params(axis='both',which='both',direction='in',bottom=True,top=True,left=True,right=True)
+        ax.tick_params(axis='both',which='major',length=axmajlen)
+        ax.tick_params(axis='both',which='minor',length=axminlen)
+        ax.tick_params(axis='both',which='both',width=axwidth)
+    ax1.set_xlim(16475, 16945)
+    ax2.set_xlim(16475, 16945)
+    ax1.xaxis.set_major_locator(ticker.MultipleLocator(50))
+    ax2.xaxis.set_major_locator(ticker.MultipleLocator(50))
+    ax1.xaxis.set_minor_locator(ticker.MultipleLocator(10))
+    ax2.xaxis.set_minor_locator(ticker.MultipleLocator(10))
+    ax11.xaxis.set_major_locator(ticker.MultipleLocator(20))
+    ax22.xaxis.set_major_locator(ticker.MultipleLocator(20))
+    ax11.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+    ax22.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+    ax11.set_xlim(16747, 16770)
+    ax22.set_xlim(16747, 16770)
+    ax1.axes.xaxis.set_ticklabels([])
+    ax11.axes.xaxis.set_ticklabels([])
+    ax11.axes.yaxis.set_ticklabels([])
+    ax22.axes.yaxis.set_ticklabels([])
+    ax1.set_ylabel(r'Flux')
+    ax2.set_ylabel(r'Norm Flux')
+    ax2.set_xlabel(r'Wavelength ($\rm \AA$)')
+    ax2.set_ylim(0.25, 1.35)
+    ax22.set_ylim(0.25, 1.35)
+    visdir = specdir5 + 'visit/apo25m/' + field + '/'
+
+    ax11.text(1.1, 1.00, r'EXPNUM    SECZ   S/N', transform=ax11.transAxes, fontsize=fsz)
+
+    ymx = np.zeros(nexp)
+    secz = np.zeros(nexp)
+    snr = np.zeros(nexp)
+    for iexp in range(nexp):
+        visdir1 = visdir + str(upl[iexp]) + '/' + str(umjd[iexp]) + '/'
+        cfile = visdir1 + 'apCframe-a-' + str(allscig['IM'][iexp]) + '.fits'
+        plsumfile = visdir1 + 'apPlateSum-' + str(upl[iexp]) + '-' + str(umjd[iexp]) + '.fits'
+        flux = fits.getdata(cfile)
+        wave = fits.getdata(cfile,4)
+        obj = fits.getdata(cfile,11)
+        g, = np.where(obj['TMASS_STYLE'] == star)
+        if len(g) > 0:
+            print(os.path.basename(cfile))
+            pl1 = fits.getdata(plsumfile,1)
+            pl2 = fits.getdata(plsumfile,2)
+            gg1, = np.where(allscig['IM'][iexp] == pl1['IM'])
+            secz[iexp] = pl1['SECZ'][gg1][0]
+            gg2, = np.where(star == pl2['TMASS_STYLE'])
+            snr[iexp] = pl2['sn'][gg2[0], gg1[0], 0]
+            w = wave[g][0]; f = flux[g][0]
+            ymxsec, = np.where((w > 16780) & (w < 16820))
+            ymx[iexp] = np.nanmax(f[ymxsec])
+
+    #print(ymx/np.nanmax(ymx))
+    ax1.set_ylim(0, np.nanmax(ymx)*1.15)
+    gd, = np.where((snr > 0) & (ymx > 0) & (ymx/np.nanmax(ymx) > 0.2))
+    sord = np.argsort(secz[gd])
+    secz = secz[gd][sord]
+    snr = snr[gd][sord]
+    upl = upl[gd][sord]
+    umjd = umjd[gd][sord]
+    allscig = allscig[gd][sord]
+    nexp = len(gd)
+
+    for iexp in range(nexp):
+        visdir1 = visdir + str(upl[iexp]) + '/' + str(umjd[iexp]) + '/'
+        cfile = visdir1 + 'apCframe-a-' + str(allscig['IM'][iexp]) + '.fits'
+        flux = fits.getdata(cfile)
+        wave = fits.getdata(cfile,4)
+        obj = fits.getdata(cfile,11)
+        g, = np.where(obj['TMASS_STYLE'] == star)
+        if len(g) > 0:
+            txt = star + r'  ($H=$' + str("%.3f" % round(obj['hmag'][g][0],3)) + ', field = ' + field + ')'
+            if iexp == 0: ax1.text(0.5, 1.02, txt, transform=ax1.transAxes, ha='center')
+            c = cmap(((iexp+1)/nexp)+cmapShift)
+            w = wave[g][0]; f = flux[g][0]
+            p = ax1.plot(w, f, color=c)
+            ax11.plot(w, f, color=c)
+            #c = p[0].get_color()
+            txt = str(allscig['IM'][iexp]) + '   ' + str("%.3f" % round(secz[iexp],3)) + '   ' + str(int(round(snr[iexp])))
+            ax11.text(1.1, 0.97-.04*iexp, txt, color=c, fontsize=fsz, transform=ax11.transAxes, va='top')
+            ax2.plot(w, f/np.nanmedian(f), color=c)
+            ax22.plot(w, f/np.nanmedian(f), color=c)
+
+    fig.subplots_adjust(left=0.073,right=0.875,bottom=0.06,top=0.96,hspace=0.08,wspace=0.1)
+    plt.savefig(plotfile)
+    plt.close('all')
+
+    return
+
+###########################################################################################
 def snhistory3():
     # snhistory3.png
     plotfile = specdir5 + 'monitor/' + instrument + '/snhistory3.png'
