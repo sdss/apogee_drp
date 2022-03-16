c6251854abce90e23e6c670f75198b2a2d7aa074
drewchojnowski <drewski@nmsu.edu>
2022-03-16 13:46:24 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -394,9 +394,12 @@ def telescopePos3(field='17049', cmap='gnuplot_r'):
 
     for i in range(nfiles):
         d = ascii.read(infiles[i])
-        p = ax1.plot(d['SECZ'], d['SN']/np.max(d['SN']), marker='o')
+        x = d['SECZ']
+        y = d['SN'] / np.max(d['SN'])
+        if y[3] < 0.8: continue
+        p = ax1.plot(x, y, marker='o')
         c = p[0].get_color()
-        ax1.scatter(d['SECZ'], d['SN']/np.max(d['SN']), marker='o', color=c, s=50)
+        ax1.scatter(x, d['SN']/np.max(d['SN']), marker='o', color=c, s=50)
 
     fig.subplots_adjust(left=0.073,right=0.985,bottom=0.06,top=0.96,hspace=0.08,wspace=0.1)
     plt.savefig(plotfile)
