31ca22c4e4237986f3cdd7efd206297249bd1376
drewchojnowski <drewski@nmsu.edu>
2022-03-23 14:11:05 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -543,7 +543,7 @@ def tellfitstats5(infile='tellfitstats2_stardata.fits'):
     ax.imshow(ret.statistic, cmap='hot_r', origin='lower')
     #ax.matshow(ret.statistic)#, cmap='hot_r', origin='lower')
 
-    fig.subplots_adjust(left=0.045,right=0.985,bottom=0.085,top=0.94,hspace=0.08,wspace=0.08)
+    #fig.subplots_adjust(left=0.045,right=0.985,bottom=0.085,top=0.94,hspace=0.08,wspace=0.08)
     plt.savefig(plotfile)
     plt.close('all')
 
