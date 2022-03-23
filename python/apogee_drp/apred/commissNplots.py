6cef54b4be0a26cc6537e8d7a53378a14207e22d
drewchojnowski <drewski@nmsu.edu>
2022-03-23 11:06:38 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -314,7 +314,7 @@ def tellfitstats1(outfile='tellfitstats2.fits', mjdstart=59146, mjdstop=59647,
             yvals = out[ploty+str(imol+1)]
             ax.scatter(xvals, yvals, marker='o', s=25, color='dodgerblue', edgecolors='k', alpha=0.6)
 
-        fig.subplots_adjust(left=0.065,right=0.99,bottom=0.05,top=0.985,hspace=0.05,wspace=0.15)
+        fig.subplots_adjust(left=0.075,right=0.985,bottom=0.05,top=0.985,hspace=0.05,wspace=0.15)
         plt.savefig(plotfile)
         plt.close('all')
 
