e8d4840f894d2ed01cb2c4b04da0d991e9923ffb
drewchojnowski <drewski@nmsu.edu>
2022-03-25 15:18:10 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -416,7 +416,7 @@ def tellspatial7(zoom=False):
             ax.tick_params(axis='both',which='minor',length=axminlen)
             ax.tick_params(axis='both',which='both',width=axwidth)
             ax.set_ylim(-0.05,1.1)
-            if zoom: ax.set_ylim(-0.01, 0.1)
+            if zoom: ax.set_ylim(-0.01, 0.2)
             if (ipar == 0) & (imol == 1): ax.set_ylabel('$\sigma$ (synth)')
             if imol == 2: ax.set_xlabel('Days since time[0]')
             if imol < 2: ax.axes.xaxis.set_ticklabels([])
