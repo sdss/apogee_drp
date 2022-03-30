355f740a4208e8bb885fcaf14b58eb8d93f34890
drewchojnowski <drewski@nmsu.edu>
2022-03-30 11:33:39 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -941,6 +941,7 @@ def tellfitstats5(infile='tellfitstats2_stardata.fits', cmap='rainbow', nbins=40
         values = gdata['FITSCALE'+str(imol+1)] - gdata['SCALE'+str(imol+1)]
         ret = stats.binned_statistic_2d(x, y, values, statistic=statistic, bins=(nbins,nbins))
         ext = [ret.x_edge[0], ret.x_edge[-1:][0], ret.y_edge[-1:][0], ret.y_edge[0]]
+        print(ext)
         im = ax.imshow(ret.statistic, cmap=cmap, aspect='auto', origin='upper', extent=ext, vmin=vmin[imol], vmax=vmax[imol])
 
         divider = make_axes_locatable(ax)
