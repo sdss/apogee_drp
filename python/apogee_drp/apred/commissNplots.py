57a9b388ec3e9a033c10d430205302245199fe6b
drewchojnowski <drewski@nmsu.edu>
2022-03-30 13:22:03 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -1004,7 +1004,7 @@ def tellfitstatsgrid(infile='tellfitstats2_stardata.fits', cmap='rainbow', nbins
             vmin = [0.95, 1.085, 0.50]
             vmax = [1.08, 1.13, 1.00]
             if doall is False: 
-                vmin = [0.97, 1.125, 0.35]
+                vmin = [0.99, 1.135, 0.35]
                 vmax = [1.13, 1.160, 0.98]
         if statistics[irow] == 'mad diff':
             vmin=[0.010, 0.010, 0.010]
