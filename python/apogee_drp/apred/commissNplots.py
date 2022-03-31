729f37cf43a25273f4ac2f4712b23e056922893a
drewchojnowski <drewski@nmsu.edu>
2022-03-31 16:33:17 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -533,7 +533,7 @@ def tellspatial4(zoom=False):
             ax.tick_params(axis='both',which='major',length=axmajlen)
             ax.tick_params(axis='both',which='minor',length=axminlen)
             ax.tick_params(axis='both',which='both',width=axwidth)
-            ax.set_ylim(-0.1,1.8)
+            ax.set_ylim(0.8,1.2)
             if zoom: ax.set_ylim(-0.05, 0.05)
             if ipar == 0: ax.set_ylabel('Factional RMS (fine grid)')
             if imol == 2: ax.set_xlabel('Days since time[0]')
