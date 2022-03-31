e01ac55061f4bb7a37f4d027e4183e1c01e12526
drewchojnowski <drewski@nmsu.edu>
2022-03-31 14:26:30 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -288,9 +288,9 @@ def tellspatialnew3(zoom=False, cmap='brg'):
             if imol > 0: ax.axes.yaxis.set_ticklabels([])
             if irow == nrows-1: ax.set_xlabel(r'$N$ tellurics')
             if irow == 0:
-                if imol == 0: ax.set_ylabel(r'RMS (fit $-$ poly)')
+                if imol == 0: ax.set_ylabel(r'fit $-$ poly')
             if irow == 1:
-                if imol == 0: ax.set_ylabel(r'RMS (fit $-$ poly)')
+                if imol == 0: ax.set_ylabel(r'fit $-$ poly')
             if irow == 0: ax.text(0.5, 1.02, molecules[imol], transform=ax.transAxes, ha='center', va='bottom')
             #if imol == 0:
             #    if ipar == 0: ax.text(0.5, 1.02, r'Constant$/$no variation', transform=ax.transAxes, ha='center', va='bottom', bbox=bboxpar)
