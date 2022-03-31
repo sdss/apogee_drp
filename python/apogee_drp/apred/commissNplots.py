db4093b259a9efd4c18d0f5140d5e12a698e6bc8
drewchojnowski <drewski@nmsu.edu>
2022-03-31 11:48:42 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -203,7 +203,7 @@ def tellspatialnew2(zoom=False, cmap='brg'):
             ax.tick_params(axis='both',which='major',length=axmajlen)
             ax.tick_params(axis='both',which='minor',length=axminlen)
             ax.tick_params(axis='both',which='both',width=axwidth)
-            #ax.set_ylim(-0.001,0.1)
+            ax.set_ylim(-0.2,0.2)
             ax.set_xlim(0,75)
             if irow == 0: ax.axes.xaxis.set_ticklabels([])
             #if imol > 0: ax.axes.yaxis.set_ticklabels([])
