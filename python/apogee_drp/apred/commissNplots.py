de437dc8187ae835b59173425aac36b6413b63af
drewchojnowski <drewski@nmsu.edu>
2022-04-25 11:54:25 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -160,6 +160,10 @@ def persist1(cmap='brg', vrad=400):
             #if imol == 0: ax.set_ylabel(r'RMS (fit $-$ poly)')
             if ichip == 0: ax.text(0.5, 1.02, chps[ichip], transform=ax.transAxes, ha='center', va='bottom')
 
+            if (iexp == 0) & (ichip == 0): ax.set_ylabel('Blue Pix')
+            if (iexp == 0) & (ichip == 0): ax.set_ylabel('Green Pix')
+            if (iexp == 0) & (ichip == 0): ax.set_ylabel('Red Pix')
+
             twod1 = fits.getdata(expdir + 'ap2D-' + chps[ichip] + '-' + str(exp1[iexp]) + '.fits')
             twod2 = fits.getdata(expdir + 'ap2D-' + chps[ichip] + '-' + str(exp1[iexp]+1) + '.fits')
 
