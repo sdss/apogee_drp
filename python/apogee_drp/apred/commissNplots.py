710f1e260e70e3cfb0663569aac42c1b99e59e6f
drewchojnowski <drewski@nmsu.edu>
2022-04-25 12:58:53 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -229,8 +229,8 @@ def tellredtests1(field='21200', conf='3922', mjd='59648', fiber='273'):
         wave = np.concatenate([wave[0,:],wave[1,:],wave[2,:]])
         ax1.plot(wave, flux+med0*0.075*(i+1), color=colors[i], linewidth=0.5, label=labels[i])
         ax2.plot(wave, flux, linewidth=0.5, color=colors[i])
-        ax3.plot(wave, ((flux/flux0))-1, color=colors[i], linewidth=0.5)
-        med = np.nanmedian((flux/flux0)-1)
+        ax3.plot(wave, 100*((flux/flux0)-1), color=colors[i], linewidth=0.5)
+        med = np.nanmedian(100*((flux/flux0)-1))
         print(med)
 
     ax1.legend(loc='upper right', labelspacing=0.5, handletextpad=0.1, fontsize=fsz, edgecolor='k', framealpha=1)
