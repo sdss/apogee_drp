b1fa7646fa1bb4d44cb97f8760212bd2a989f711
drewchojnowski <drewski@nmsu.edu>
2022-04-25 12:47:02 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -191,7 +191,7 @@ def tellredtests1(field='21200', conf='3922', mjd='59648', fiber='273'):
     origvis = os.environ.get('APOGEE_REDUX')+'/daily/visit/apo25m/'+field+'/'+conf+'/'+mjd+'/'+origvis0
 
     colors = ['r','b','orange','violet','seagreen']
-    labels = ['red 15', 'blue 15', 'bright 15', 'faint 15', '"best" 15']
+    labels = np.array(['red 15', 'blue 15', 'bright 15', 'faint 15', '"best" 15'])
 
     visitxmin = 15120;   visitxmax = 16960;    visitxspan = visitxmax - visitxmin
     fig = plt.figure(figsize=(28,24))
@@ -226,11 +226,11 @@ def tellredtests1(field='21200', conf='3922', mjd='59648', fiber='273'):
         flux = np.concatenate([flux[0,:],flux[1,:],flux[2,:]])
         wave = fits.getdata(vis,4)
         wave = np.concatenate([wave[0,:],wave[1,:],wave[2,:]])
-        ax1.plot(wave, flux+med0*0.075*(i+1), linewidth=0.5)
+        ax1.plot(wave, flux+med0*0.075*(i+1), linewidth=0.5, label=labels[i])
         ax2.plot(wave, flux, linewidth=0.5)
-        ax3.plot(wave, 100*((flux-flux0)/med0), colors[i], linewidth=0.5, label=labels[i])
+        ax3.plot(wave, 100*((flux-flux0)/med0), colors[i], linewidth=0.5)
 
-    ax1.legend('upper right', ncol=2, labelspacing=0.5, handletextpad=-0.1, fontsize=fsz, edgecolor='k', framealpha=1)
+    ax1.legend('upper right', labelspacing=0.5, handletextpad=-0.1, fontsize=fsz, edgecolor='k', framealpha=1)
 
     fig.subplots_adjust(left=0.07,right=0.985,bottom=0.057,top=0.96,hspace=0.08,wspace=0.05)
     plt.savefig(plotfile)
