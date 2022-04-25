afd6d06c22ee510aae6c22e7672f4d71cb348a8c
drewchojnowski <drewski@nmsu.edu>
2022-04-25 12:37:39 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -213,6 +213,7 @@ def tellredtests1(field='21200', conf='3922', mjd='59648', fiber='273'):
     flux0 = np.concatenate([flux[0,:],flux[1,:],flux[2,:]])
     wave = fits.getdata(origvis,4)
     wave0 = np.concatenate([wave[0,:],wave[1,:],wave[2,:]])
+    med0 = np.nanmedian(flux0)
     ax1.plot(wave0, flux0, 'k')
     for i in range(4):
         vis = os.environ.get('APOGEE_REDUX')+'/caltests1.0/visit/apo25m/'+field+'_0'+str(i+1)+'/'+conf+'/'+mjd+'/'+origvis0.replace('daily','caltests1.0')
@@ -221,7 +222,7 @@ def tellredtests1(field='21200', conf='3922', mjd='59648', fiber='273'):
         wave = fits.getdata(vis,4)
         wave = np.concatenate([wave[0,:],wave[1,:],wave[2,:]])
         ax1.plot(wave, flux)
-        ax2.plot(wave, flux-flux0)
+        ax2.plot(wave, (flux-flux0)/med0)
 
 
     fig.subplots_adjust(left=0.07,right=0.985,bottom=0.057,top=0.96,hspace=0.08,wspace=0.05)
