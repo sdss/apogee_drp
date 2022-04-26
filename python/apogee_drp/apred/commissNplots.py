fe9c65d641eb8ff4ff85659a4649f810927eaabb
drewchojnowski <drewski@nmsu.edu>
2022-04-26 12:26:49 -0600

    updating commissNplots

--- python/apogee_drp/apred/commissNplots.py
+++ python/apogee_drp/apred/commissNplots.py
@@ -301,6 +301,7 @@ def tellredtests2(field='21200', conf='3922', mjd='59648', fiber='273'):
             rowspan = 2
             if i == nrows-1: rowspan = 1
             if i > 0: rownum = rownum+rowspan
+            pdb.set_trace()
             ax = plt.subplot2grid((9,3), (rownum,j), rowspan=2)
             ax.set_xlim(xxmin[j], xxmax[j])
             if i < nrows-1: ax.axes.xaxis.set_ticklabels([])
