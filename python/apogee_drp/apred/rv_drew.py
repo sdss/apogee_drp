77699e49913d13dda5ab1cb1dec55ae9ff30291f
drewchojnowski <drewski@nmsu.edu>
2022-04-29 13:39:17 -0600

    updating rv_drew

--- python/apogee_drp/apred/rv_drew.py
+++ python/apogee_drp/apred/rv_drew.py
@@ -216,7 +216,7 @@ def doppler_rv(star,apred,telescope,mjd=None,nres=[5,4.25,3.5],windows=None,twea
     # Add STARVER                                                                                                                                             
     starvisits['starver'] = starver
     # Flag all visits as RV_FAIL to start with, will remove if they worked okay
-    starvisits['starflag'] |= starmask.getval('RV_FAIL')
+    starvisits['STARFLAG'] |= starmask.getval('RV_FAIL')
     # Initialize visit RV tags
     for col in ['vtype','vrel','vrelerr','vheliobary','bc','chisq','rv_teff','rv_tefferr','rv_logg','rv_loggerr','rv_feh','rv_feherr']:
         if col == 'vtype':
@@ -265,7 +265,7 @@ def doppler_rv(star,apred,telescope,mjd=None,nres=[5,4.25,3.5],windows=None,twea
             continue
         visits.append(vind)
         # Remove RV_FAIL that we added above
-        starvisits['starflag'][vind] &= ~starmask.getval('RV_FAIL')
+        starvisits['STARFLAG'][vind] &= ~starmask.getval('RV_FAIL')
         # Add Doppler outputs
         starvisits['vrel'][vind] = v['vrel']
         starvisits['vrelerr'][vind] = v['vrelerr']
