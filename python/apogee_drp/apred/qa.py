ee2aa0ec44dc0bf2d30a275dbca4b0db138a4ee3
drewchojnowski <drewski@nmsu.edu>
2022-05-04 13:57:53 -0600

    updating qa

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1291,8 +1291,8 @@ def makeObsPlots(load=None, ims=None, imsReduced=None, plate=None, mjd=None, ins
                 pp, = np.where(hmdif == np.nanmin(hmdif))
                 diff[q] = Vsum['SNR'][gd][q] - 10**yarrnew[pp][0]
             pp, = np.where(np.absolute(diff) < 2*np.nanstd(diff))
-            theta = np.polyfit(hmagarr[gd][pp], np.log10(Vsum['SNR'][gd][pp]), 2)
-            xarrnew = np.linspace(np.nanmin(hmagarr[gd][pp]), np.nanmax(hmagarr[gd][pp]), 5000)
+            theta = np.polyfit(hmagarr[gd][pp], np.log10(Vsum['SNR'][gd][pp]), 1)
+            xarrnew = np.linspace(np.nanmin(hmagarr[gd]), np.nanmax(hmagarr[gd]), 5000)
             yarrnew = theta[1] + theta[0] * xarrnew
 
             #pdb.set_trace()
