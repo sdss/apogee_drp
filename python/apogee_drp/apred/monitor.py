bf1c99b5376eb42c347bd3847fd873e208d17d9d
drewchojnowski <drewski@nmsu.edu>
2022-05-06 09:57:37 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -2317,10 +2317,9 @@ def getSnrStruct2(data1=None, data2=None, iexp=None, field=None, sumfile=None):
     outstr['ALTSN'] =     data1['ALTSN'][iexp]
     outstr['NSN'] =       data1['NSN'][iexp]
     outstr['SNRATIO'] =   data1['SNRATIO'][iexp]
-    outstr['HMAG'][0][fiberid-1] = data2['HMAG']
-    outstr['FIBERID'][0][fiberid-1] = data2['FIBERID']
-    pdb.set_trace()
-    outstr['SNFIBER'][iexp, fiberid-1] =   data2['SN'][:, iexp, :]
+    outstr['HMAG'][0, fiberid-1] = data2['HMAG']
+    outstr['FIBERID'][0, fiberid-1] = data2['FIBERID']
+    outstr['SNFIBER'][0, fiberid-1, :] =   data2['SN'][:, iexp, :]
     jj=0
     for magbin in magbins:
         g, = np.where((data2['HMAG'] >= magbin-magrad) & (data2['HMAG'] < magbin+magrad) & (np.isnan(data2['HMAG']) == False))
