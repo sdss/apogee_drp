ece226361ed537b876b3e96ddf440c0c7c9b979b
drewchojnowski <drewski@nmsu.edu>
2022-05-12 12:30:48 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -2208,11 +2208,11 @@ def makeVisHTML(load=None, plate=None, mjd=None, survey=None, apred=None, telesc
                 else:
                     vishtml.write('<TD align ="center" BGCOLOR="grey">-1%\n')
 
-                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>VHELIO</B><BR><BR>' + str("%.1f" % round(vhelio,1)) + '</FONT>')
-                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>NCOMP</B><BR><BR>' + str(ncomp) + '</FONT>')
-                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>TEFF</B><BR><BR>' + str(int(round(rvteff))) + '</FONT>')
-                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>LOGG</B><BR><BR>' + str("%.3f" % round(rvlogg,3)) + '</FONT>')
-                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>[FE/H]</B><BR><BR>' + str("%.3f" % round(rvfeh,3)) + '</FONT>')
+                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>VHELIO</B><BR><BR><BR>' + str("%.1f" % round(vhelio,1)) + '</FONT>')
+                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>NCOMP</B><BR><BR><BR>' + str(ncomp) + '</FONT>')
+                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>TEFF</B><BR><BR><BR>' + str(int(round(rvteff))) + '</FONT>')
+                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>LOGG</B><BR><BR><BR>' + str("%.3f" % round(rvlogg,3)) + '</FONT>')
+                vishtml.write('<TD align ="center"><FONT COLOR="' + vcol + '"><B>[FE/H]</B><BR><BR><BR>' + str("%.3f" % round(rvfeh,3)) + '</FONT>')
             else:
                 snr = '-9.9'
                 relsnr = '-1%'
