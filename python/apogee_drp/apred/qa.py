882623f99aa873b145b3e14eda54eac2a3b03a64
drewchojnowski <drewski@nmsu.edu>
2022-05-12 13:13:29 -0600

    updating qa.py

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -124,7 +124,6 @@ def apqaALL(mjdstart='59146', observatory='apo', apred='daily', makeplatesum=Tru
     for i in range(ndirs): allmjd[i] = mjdDirs[i].split('/')[-1]
     gd, = np.where(allmjd != 'plots')
     umjd = np.unique(allmjd[gd])
-    pdb.set_trace()
     gd, = np.where(umjd == mjdstart)
     umjd = umjd[gd[0]:]
     umjd = umjd[::-1]
