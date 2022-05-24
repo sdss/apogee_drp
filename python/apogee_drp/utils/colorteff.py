aa040ae06d064af88af8d07cc8dbf674fe99a5cf
DrewChojnowski <drewski@nmsu.edu>
2022-05-24 10:27:00 -0600

    updating colorteff

--- python/apogee_drp/utils/colorteff.py
+++ python/apogee_drp/utils/colorteff.py
@@ -184,6 +184,7 @@ def solve(tab):
     # Observed magnitudes/colors
     obsmag = np.zeros(len(bands),float)
     for i,b in enumerate(bands):
+        import pdb; pdb.set_trace()
         obsmag[i] = tab[b]
     obsmagerr = np.zeros(len(bands),float)
     for i,b in enumerate(bands):
