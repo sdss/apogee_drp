1585462bdb757d1ae070150abb7091e9b6122419
drewchojnowski <drewski@nmsu.edu>
2022-05-31 13:27:09 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -919,9 +919,9 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
         if (os.path.exists(plotfile) == False) | (clobber == True):
             print("----> monitor: Making " + os.path.basename(plotfile))
 
-            snbins = [0,5]
-            magmin = '7.0'
-            magmax = '9.0'
+            snbins = [9,10]
+            magmin = '10.6'
+            magmax = '11.0'
 
             plateobs, = np.where(allsnr['MJD'] < 59500)
             allsnrplate = allsnr[plateobs]
