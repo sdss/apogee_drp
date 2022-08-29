2550bdbf823c15f93de1f74c9e4e5c1818d4f3bc
drewchojnowski <drewski@nmsu.edu>
2022-08-29 09:51:06 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -300,20 +300,20 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
 
             files.sort()
             files = np.array(files)
-            nfiles=len(files)
+            nfiles = len(files)
 
             # Loop over SDSS-V files and add them to output structure
             for i in range(nfiles):
                 data = fits.open(files[i])[1].data
-                pdb.set_trace()
-                check, = np.where(data['DATEOBS'][0] == outstr['DATEOBS'])
-                if len(check) > 0:
-                    #print("---->    monitor: skipping " + os.path.basename(files[i]))
-                    continue
-                else:
-                    print("---->    monitor: adding " + os.path.basename(files[i]) + " to master file")
-                    newstr = getExpStruct(data)
-                    outstr = np.concatenate([outstr, newstr])
+                if d['DATEOBS'].shape[0] != 0:
+                    check, = np.where(data['DATEOBS'] == outstr['DATEOBS'])
+                    if len(check) > 0:
+                        #print("---->    monitor: skipping " + os.path.basename(files[i]))
+                        continue
+                    else:
+                        print("---->    monitor: adding " + os.path.basename(files[i]) + " to master file")
+                        newstr = getExpStruct(data)
+                        outstr = np.concatenate([outstr, newstr])
 
             Table(outstr).write(outfile, overwrite=True)
             print("----> monitor: Finished making " + os.path.basename(outfile))
