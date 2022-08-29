cd1c6619beb11819da359c590c524d74c138eaa6
drewchojnowski <drewski@nmsu.edu>
2022-08-29 09:51:28 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -305,7 +305,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
             # Loop over SDSS-V files and add them to output structure
             for i in range(nfiles):
                 data = fits.open(files[i])[1].data
-                if d['DATEOBS'].shape[0] != 0:
+                if data['DATEOBS'].shape[0] != 0:
                     check, = np.where(data['DATEOBS'] == outstr['DATEOBS'])
                     if len(check) > 0:
                         #print("---->    monitor: skipping " + os.path.basename(files[i]))
