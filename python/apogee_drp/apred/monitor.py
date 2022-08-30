c117229b76221d19671cc7c19a219cdaa9f1cc04
drewchojnowski <drewski@nmsu.edu>
2022-08-30 13:14:43 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -142,7 +142,6 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                         for j in range(144,156):
                             data = fits.getdata(files[i],j)
                             if data['FIBER'][0] == 150: 
-                                pdb.set_trace()
                                 struct1['CENT'] = np.squeeze(data['CENT'])[1000]
                         outstr = np.concatenate([outstr, struct1])
 
