ffc4c3f4867b7b1c3e599c9c3054553b40b05de2
drewchojnowski <drewski@nmsu.edu>
2022-11-30 12:21:04 -0700

    debugging qa

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -4272,14 +4272,13 @@ def makeExpFits(instrument=None, apodir=None, apred=None, load=None, mjd=None, c
                 struct['CARTID'][i] =    hdr['CARTID']
                 struct['RA'][i] =        hdr['RA']
                 struct['DEC'][i] =       hdr['DEC']
-            except:
-                nothing = 0
+                struct['ALT'][i] =       hdr['ALT']
+            except: pass
             if hdr['SEEING'] != 'NAN.0':
                 struct['SEEING'][i] = hdr['SEEING']
             else:
                 print("----> makeExpFits: seeing is 'NAN.0' for exposure " + imnum + ". Setting to -9.999")
                 struct['SEEING'][i] = -9.999
-            struct['ALT'][i] =       hdr['ALT']
             try:
                 struct['QRTZ'][i] =      hdr['LAMPQRTZ']
                 struct['THAR'][i] =      hdr['LAMPTHAR']
