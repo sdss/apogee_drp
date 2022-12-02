39fdfcc17b0ee83df05093b0a3c18ba6caeb24d5
drewchojnowski <drewski@nmsu.edu>
2022-12-02 10:36:48 -0700

    updating qa

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -1593,6 +1593,7 @@ def makeObsPlots(load=None, ims=None, imsReduced=None, plate=None, mjd=None, ins
             ax.bar(xarr, tput[::-1], label=chiplab[ichip]+'\n'+'chip', color=chiplab[ichip], width=1, zorder=10)
             if ichip == 0:
                 for imtp in len(mtpLabelPos):
+                    pdb.set_trace()
                     ax.text(mtpLabelPos[imtp], 1.04, 'MTP '+str(imtp+1), ha='center', fontsize=fontsize*0.75)
 
 
