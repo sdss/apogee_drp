17f3b5c3afd8e57bd1a54c46b72e784adf2fd06c
drewchojnowski <drewski@nmsu.edu>
2023-02-02 08:58:01 -0700

    updating apogeedrp

--- python/apogee_drp/plan/apogeedrp.py
+++ python/apogee_drp/plan/apogeedrp.py
@@ -2931,7 +2931,7 @@ def runqa(load,mjds,slurmpars,clobber=False,logger=None):
         errfile = logfile.replace('.log','.err')
         cmd = 'apqa {0} {1} --apred {2} --telescope {3} --plate {4}'.format(mjd,observatory,apred,telescope,plate)
         #cmd += ' --masterqa False --starhtml False --starplots False --nightqa False --monitor False'
-        cmd += ' --masterqa False --starhtml True --starplots True --nightqa False --monitor False'
+        cmd += ' --masterqa False --starhtml False --starplots False --nightqa False --monitor False'
         logger.info('Command : '+cmd)
         logger.info('Logfile : '+logfile)
         queue.append(cmd, outfile=logfile, errfile=errfile)
