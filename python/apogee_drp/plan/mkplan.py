9976f7fbd0c6869497466d81e947dae4ae9a30e6
drewchojnowski <drewski@nmsu.edu>
2023-02-16 11:44:45 -0700

    updating mkplan.py

--- python/apogee_drp/plan/mkplan.py
+++ python/apogee_drp/plan/mkplan.py
@@ -912,12 +912,12 @@ def make_mjd5_yaml(mjd,apred,telescope,clobber=False,logger=None):
             oldmjd = mjd-cntr
             expinfo1 = info.expinfo(observatory=observatory,mjd5=oldmjd)
             qachk1 = check.check(expinfo1['num'],apred,telescope,verbose=False)
-            domeind, = np.where((expinfo1['exptype']=='DOMEFLAT') & (qachk1['okay']==True) )
+            domeind, = np.where((expinfo1['exptype']=='DOMEFLAT') & (qachk1['okay']==True))
             dome = list(expinfo1['num'][domeind].astype(int))
             cntr +=1
         #domepluggroup = expinfo1['pluggroup'][domeind]
         logger.info('Using dome flats from MJD '+str(oldmjd))
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
 
     # Check which apPSF and apFlux files exist and can be used for calibration files
     # Which domeflat apPSF files exist
@@ -926,6 +926,7 @@ def make_mjd5_yaml(mjd,apred,telescope,clobber=False,logger=None):
         psffile = load.filename('PSF',num=dome[j],chips=True)
         psffiles = [psffile.replace(load.prefix+'PSF-',load.prefix+'PSF-'+ch+'-') for ch in chips]
         exist = [os.path.exists(pf) for pf in psffiles]
+        import pdb; pdb.set_trace()
         if np.sum(np.array(exist))==3:
             psfdome_exist[j] = True
     gddome, = np.where(psfdome_exist == True)
