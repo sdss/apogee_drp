6d208e5a295f047d3fd3190c019c834a93358466
drewchojnowski <drewski@nmsu.edu>
2023-04-10 10:46:55 -0600

    updating qa

--- python/apogee_drp/apred/qa.py
+++ python/apogee_drp/apred/qa.py
@@ -3407,8 +3407,8 @@ def makeNightQA(load=None, mjd=None, telescope=None, apred=None):
     pfile2 = glob.glob(os.path.dirname(platefiles[0])+'/plots/'+load.prefix+'Tput-*.png')
     relpath1 = '../../../../visit/'+telescope+'/'+field+'/'+plate+'/'+mjd+'/plots/'+os.path.basename(pfile1[0])
     relpath2 = '../../../../visit/'+telescope+'/'+field+'/'+plate+'/'+mjd+'/plots/'+os.path.basename(pfile2[0])
-    html.write('<TR><TD><A HREF='+relpath1+' target="_blank"><IMG SRC='+relpath1+' HEIGHT=200></A>\n')
-    html.write('<TD><A HREF='+relpath2+' target="_blank"><IMG SRC='+relpath2+' HEIGHT=200></A>\n')
+    html.write('<TR><TD><A HREF='+relpath1+' target="_blank"><IMG SRC='+relpath1+' HEIGHT=225></A>\n')
+    html.write('<TD><A HREF='+relpath2+' target="_blank"><IMG SRC='+relpath2+' HEIGHT=225></A>\n')
     html.write('</TABLE>\n')
 
     # Show throughput qa plots from first and final visit of the night
@@ -3420,15 +3420,9 @@ def makeNightQA(load=None, mjd=None, telescope=None, apred=None):
     pfile2 = glob.glob(os.path.dirname(platefiles[-1])+'/plots/'+load.prefix+'Tput-*.png')
     relpath1 = '../../../../visit/'+telescope+'/'+field+'/'+plate+'/'+mjd+'/plots/'+os.path.basename(pfile1[0])
     relpath2 = '../../../../visit/'+telescope+'/'+field+'/'+plate+'/'+mjd+'/plots/'+os.path.basename(pfile2[0])
-    html.write('<TR><TD><A HREF='+relpath1+' target="_blank"><IMG SRC='+relpath1+' HEIGHT=200></A>\n')
-    html.write('<TD><A HREF='+relpath2+' target="_blank"><IMG SRC='+relpath2+' HEIGHT=200></A>\n')
+    html.write('<TR><TD><A HREF='+relpath1+' target="_blank"><IMG SRC='+relpath1+' HEIGHT=225></A>\n')
+    html.write('<TD><A HREF='+relpath2+' target="_blank"><IMG SRC='+relpath2+' HEIGHT=225></A>\n')
     html.write('</TABLE>\n')
-
-
-    html.close()
-
-    return
-    #pdb.set_trace()
 
 #    print,'wavehtml...'
 #    wavefile = caldir + 'wave/html/wave' + mjd + '.html'
