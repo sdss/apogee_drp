af4e1ae7d1ee6af5bafa05e8d2bb0f9a61ba316d
drewchojnowski <drewski@nmsu.edu>
2023-04-14 11:17:05 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -2203,7 +2203,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
 
                 for iyear in range(nyears):
                     ax.axvline(x=yearjd[iyear], color='k', linestyle='dashed', alpha=alf)
-                    if ichip == 0: ax.text(yearjd[iyear], ymax-yspan*0.25, cyears[iyear], ha='center', va='top', fontsize=fsz80)
+                    if ichip == 0: ax.text(yearjd[iyear], ymax+yspan*0.35, cyears[iyear], ha='center', va='bottom', fontsize=fsz80)
 
                 for ibias in range(4):
                     ax.semilogy(caljd, gdcal['MEAN'][:, ibias, ichip], marker='o', ms=3, alpha=alf, 
@@ -2257,7 +2257,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
 
                 for iyear in range(nyears):
                     ax.axvline(x=yearjd[iyear], color='k', linestyle='dashed', alpha=alf)
-                    if ichip == 0: ax.text(yearjd[iyear], ymax-yspan*0.25, cyears[iyear], ha='center', va='top', fontsize=fsz80)
+                    if ichip == 0: ax.text(yearjd[iyear], ymax+yspan*0.3, cyears[iyear], ha='center', va='bottom', fontsize=fsz80)
 
                 for ibias in range(4):
                     ax.semilogy(caljd, gdcal['SIG'][:, ibias, ichip], marker='o', ms=3, alpha=alf, 
