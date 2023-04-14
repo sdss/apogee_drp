df9ba1f97be5a310b647e3ca18bfd39725496c25
drewchojnowski <drewski@nmsu.edu>
2023-04-14 09:12:27 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -2301,7 +2301,11 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
             cb1 = colorbar(sc1, cax=cax, orientation="vertical")
             cax.minorticks_on()
             cax.yaxis.set_major_locator(ticker.MultipleLocator(15))
-            ax1.text(1.072, 0.5, r'Moon Distance (deg.)',ha='left', va='center', rotation=-90, transform=ax1.transAxes)
+            cax.tick_params(axis='both',which='major',length=axmajlen)
+            cax.tick_params(axis='both',which='minor',length=axminlen)
+            cax.tick_params(axis='both',which='both',width=axthick)
+            for axis in ['top','bottom','left','right']: cax.spines[axis].set_linewidth(axthick)
+            ax1.text(1.076, 0.5, r'Moon Distance (deg.)',ha='left', va='center', rotation=-90, transform=ax1.transAxes)
 
             ax_divider = make_axes_locatable(ax2)
             cax = ax_divider.append_axes("right", size="2%", pad="1%")
@@ -2312,7 +2316,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
             cax.tick_params(axis='both',which='minor',length=axminlen)
             cax.tick_params(axis='both',which='both',width=axthick)
             for axis in ['top','bottom','left','right']: cax.spines[axis].set_linewidth(axthick)
-            ax2.text(1.072, 0.5, r'Zeropoint (cloudiness)',ha='left', va='center', rotation=-90, transform=ax2.transAxes)
+            ax2.text(1.076, 0.5, r'Zeropoint (cloudiness)',ha='left', va='center', rotation=-90, transform=ax2.transAxes)
 
             fig.subplots_adjust(left=0.045,right=0.935,bottom=0.07,top=0.96,hspace=0.17,wspace=0.00)
             plt.savefig(plotfile)
