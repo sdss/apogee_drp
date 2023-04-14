f61c1357e7811260ca05f8b4a3a1505e78ec3f70
drewchojnowski <drewski@nmsu.edu>
2023-04-14 14:05:43 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -509,8 +509,8 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
     html.write('<li> <a href=#detectors> Detectors\n')
     html.write('<li> <a href=#sky> Sky brightness\n')
    # html.write('<li> <a href=' + instrument + '/flatflux.html target="_blank">Flat Field Relative Flux Plots</a>\n')
-    html.write('<li> <a href=' + instrument + '/expInfo_'+instrument+'.html target="_blank">Cal exposure info table</a> (external page)\n')
     html.write('<li> <a href=#zero>Plate zeropoints</a>\n')
+    html.write('<li> <a href=' + instrument + '/expInfo_'+instrument+'.html target="_blank">Cal exposure info table</a> (external page)\n')
     html.write('</ul>\n')
     html.write('<HR>\n')
 
