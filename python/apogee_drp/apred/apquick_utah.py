6ec0c179768ac2d8dd412d07bf84e9561dd2ad0c
drewchojnowski <drewski@nmsu.edu>
2023-04-17 15:14:18 -0600

    updating apquick_utah

--- python/apogee_drp/apred/apquick_utah.py
+++ python/apogee_drp/apred/apquick_utah.py
@@ -934,7 +934,6 @@ def runquick(filename,hdulist=None,framenum=None,mjd=None,load=None,apred='daily
     else:
         subspec = spec
     # Create the S/N catalog
-    pdb.set_trace()
     cat = snrcat(subspec,plugmap)
     print('Mean S/N = %5.2f' % np.mean(cat['snr']))
     if exptype != 'OBJECT':
