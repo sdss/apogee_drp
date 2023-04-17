3aec7e59244d9993f527d99d4efd9ccf737fbb84
drewchojnowski <drewski@nmsu.edu>
2023-04-17 15:12:42 -0600

    updating apquick_utah

--- python/apogee_drp/apred/apquick_utah.py
+++ python/apogee_drp/apred/apquick_utah.py
@@ -907,6 +907,7 @@ def runquick(filename,hdulist=None,framenum=None,mjd=None,load=None,apred='daily
     except:
         plugfile = plugdir4+'plPlugMapM-'+str(head['PLATEID'])+'-'+mjd+'-'+str(head['MAPID']).zfill(2)+'.par'
 
+    pdb.set_trace()
     # Load plugmap/fibermap file
     if plugfile is not None:
         if os.path.exists(plugfile) is False:
