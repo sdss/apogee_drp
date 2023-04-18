6b74afbc45fa89817636e62a0abc279665cf8b40
drewchojnowski <drewski@nmsu.edu>
2023-04-18 14:01:20 -0600

    updating apquick_utah

--- python/apogee_drp/apred/apquick_utah.py
+++ python/apogee_drp/apred/apquick_utah.py
@@ -953,6 +953,9 @@ def runquick(filename,hdulist=None,framenum=None,mjd=None,load=None,psfnums=None
         plugmap1 = yanny.yanny(plugfile1)#,np=True)
         plugmap2 = plugmap1#,np=True)
         fibermap1 = plugmap1['FIBERMAP']
+        g, = np.where((fibermap1['fiberType']=='APOGEE') & (fibermap1['assigned']==1) & (fibermap1['on_target']==1) & (fibermap1['valid']==1) & 
+                      (fibermap1['spectrgraphID']==2) & (fibermap['fiberId']>=0))
+        fibermap1 = fibermap1[g]
         fibermap2 = fibermap1
     else: 
         plugmap1 = yanny.yanny(plugfile1,np=True)
