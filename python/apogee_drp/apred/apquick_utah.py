b543da6277f850cb43bda121212b584e24646f9a
drewchojnowski <drewski@nmsu.edu>
2023-04-20 09:53:25 -0600

    updating apquick_utah

--- python/apogee_drp/apred/apquick_utah.py
+++ python/apogee_drp/apred/apquick_utah.py
@@ -207,7 +207,7 @@ def makesumfiles(telescope='lco25m',apred='daily'):
                    ('ZERONORM',             np.float64),
                    ('EXPTYPE',              np.str),
                    ('DITHERPOS',            np.float64),
-                   ('APOGEE_ID',            np.str,(300)),
+                   #('APOGEE_ID',            np.str,(300)),
                    ('CATALOGID',            np.int32,(300)),
                    ('RA',                   np.float64,(300)),
                    ('DEC',                  np.float64,(300)),
@@ -238,8 +238,8 @@ def makesumfiles(telescope='lco25m',apred='daily'):
         outstr['ZERONORM'][i] = d1['zeronorm'][0]
         outstr['EXPTYPE'][i] = d1['exptype'][0]
         outstr['DITHERPOS'][i] = d1['ditherpos'][0]
-        pdb.set_trace()
-        outstr['APOGEE_ID'][i] = d2['apogee_id']
+        #pdb.set_trace()
+        ##outstr['APOGEE_ID'][i] = np.array(d2['apogee_id'])
         outstr['CATALOGID'][i] = d2['catalogid']
         outstr['RA'][i] = d2['ra']
         outstr['DEC'][i] = d2['dec']
