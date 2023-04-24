01816ee8a804f5653f618716e6c1fbb3d3a4bccf
drewchojnowski <drewski@nmsu.edu>
2023-04-24 13:53:46 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -1687,7 +1687,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                             yvals = yvals0[g3]
                         sc1 = ax.scatter(xxvals, yvals, marker='o', s=markersz*5, c=colors[j], label=labels[j])
                         
-                    ax.legend(loc='upper left', ncol=1, labelspacing=0.5, handletextpad=0.5, markerscale=1, columnspacing=0.3,
+                    ax.legend(loc='upper left', ncol=1, labelspacing=0.5, handletextpad=0.5, markerscale=4, columnspacing=0.3,
                               fontsize=fsz80, edgecolor='k', framealpha=1, borderaxespad=0.8, borderpad=0.6)
 
 
