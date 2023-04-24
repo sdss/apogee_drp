5e3933794e85e3ab2d9cef87052de94c0d689903
drewchojnowski <drewski@nmsu.edu>
2023-04-24 12:43:25 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -1425,9 +1425,9 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
 
             qdata0 = fits.getdata(qfile)
             g, = np.where((qdata0['SNR_FID_1'] > 0) & 
-                          (qdata0['NREAD'] > 40)  & 
-                          (qdata0['NREAD'] < 50) & 
-                          (qdata0['N_10pt0_11pt5'] > 10) & 
+                          #(qdata0['NREAD'] > 40)  & 
+                          #(qdata0['NREAD'] < 50) & 
+                          #(qdata0['N_10pt0_11pt5'] > 10) & 
                           (qdata0['SEEING'] > 0))
             qdata = qdata0[g]
             x = qdata['mjd']
