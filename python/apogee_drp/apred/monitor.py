7bb4c71000ae4547e93bd87e3f44e63a558d44b7
drewchojnowski <drewski@nmsu.edu>
2023-04-25 15:20:37 -0600

    updating monitor

--- python/apogee_drp/apred/monitor.py
+++ python/apogee_drp/apred/monitor.py
@@ -1262,7 +1262,7 @@ def monitor(instrument='apogee-n', apred='daily', clobber=True, makesumfiles=Tru
                     #ax.text(0.02, 0.95, chip.capitalize() + ' Chip', transform=ax.transAxes, fontsize=fsz80, ha='left', va='top', color=chip, bbox=bboxpar)
 
                     if irow == 0:
-                        sc1 = ax.scatter(xxvals, yvals, marker='o', s=markersz*2, c=cvals, cmap='rainbow', vmin=0.5, vmax=1.5)
+                        sc1 = ax.scatter(xxvals, yvals, marker='o', s=markersz*2, c=cvals, cmap='rainbow', vmin=0.3, vmax=1.2)
                     else:
                         sc1 = ax.scatter(xxvals, yvals, marker='o', s=markersz*2, c=cvals, cmap='rainbow')
 
