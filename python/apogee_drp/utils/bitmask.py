8013a1b94addc9742bceb38f4bed49bca74458ef
David Nidever <dnidever@montana.edu>
2026-07-23 09:39:12 -0600

    a couple more updates from 1.6

--- python/apogee_drp/utils/bitmask.py
+++ python/apogee_drp/utils/bitmask.py
@@ -276,17 +276,17 @@ class PixelBitMask(BitMask) :
     BitMask class for APOGEE pixel bitmask (APOGEE_PIXMASK)
     '''
     name=(['BADPIX','CRPIX','SATPIX','UNFIXABLE','BADDARK','BADFLAT','BADERR','NOSKY',
-          'LITTROW_GHOST','PERSIST_HIGH','PERSIST_MED','PERSIST_LOW','SIG_SKYLINE','SIG_TELLURIC','NOT_ENOUGH_PSF','',
+          'LITTROW_GHOST','PERSIST_HIGH','PERSIST_MED','PERSIST_LOW','SIG_SKYLINE','SIG_TELLURIC','NOT_ENOUGH_PSF','VERY_NEGATIVE',
           'FERRE_MASK','','','','','','','',
           '','','','','','','',''])
 
     level=([1,1,1,1,1,1,1,1,
-            0,0,0,0,0,0,1,0,
+            0,0,0,0,0,0,1,1,
             0,0,0,0,0,0,0,0,
             0,0,0,0,0,0,0,0])
 
     maskcontrib=([0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,
-                 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0,
+                 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,
                  0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0,
                  0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0])
 
@@ -306,7 +306,7 @@ class PixelBitMask(BitMask) :
      'Pixel falls near sky line that has significant flux compared with object',
      'Pixel falls near telluric line that has significant absorption',
      'Less than 50 percent PSF in good pixels',
-     '',
+     'Very negative flux, flux < -5*err',
      'Pixel masked by FERRE mask < 0.001',
      '',
      '',
--- python/apogee_drp/utils/lsf.py
+++ python/apogee_drp/utils/lsf.py
@@ -5,6 +5,7 @@ import doppler
 from doppler import lsf as doplsf
 from scipy.optimize import curve_fit
 #from .norm import apStarWave
+import math
 
 logw0=4.179
 dlogw=6.e-6
@@ -14,6 +15,17 @@ def apStarWave() :
     """
     return 10.**(logw0+np.arange(nw_apStar)*dlogw)
     
+#def wave2pix(self,w,extrapolate=True,order=0):
+#    if self.wave is None:
+#        raise Exception("No wavelength information")
+#    if self.ndim==2:
+#        # Order is always the second dimension
+#        return utils.w2p(self.wave[:,order],w,extrapolate=extrapolate)
+#    else:
+#        return utils.w2p(self.wave,w,extrapolate=extrapolate)
+
+    
+
 
 class LsfGHFitter:
     def __init__(self,wave,lsfpars):
@@ -42,8 +54,9 @@ class LsfGHFitter:
 
     def loadcoefs(self,coefs):
         """ Load coefficients into the lsf parameters array."""
-
         lsfpars = self.lsf.pars.copy()
+        if lsfpars.ndim==1:
+            lsfpars = np.atleast_2d(lsfpars).T
         # Stuff in GH coefficients
         ghcoefs = coefs[0:self.nghcoefs]
         lsfpars[self.ghcoef0:self.ghcoef1+1,0] = ghcoefs
@@ -51,18 +64,19 @@ class LsfGHFitter:
         wcoefs = coefs[self.nghcoefs:]
         lsfpars[self.wcoef0:self.wcoef1+1,0] = wcoefs
         return lsfpars
+
         
-    def model(self,x,*args,flatten=True):
+    def model(self,x,*args,ravel=True):
         """ Create the LSF model."""
         coef = np.array(args)
         lsfpars = self.loadcoefs(coef)
         self.lsf.pars = lsfpars
-        
+        print(lsfpars.ravel())        
         # Generate the LSF model
         lsfmodel = self.lsf.anyarray(x,xtype='pixel',order=0,nlsf=15,original=True)
-
-        if flatten==True:
-            return lsfmodel.flatten()   # must be 1D for 
+        
+        if ravel==True:
+            return lsfmodel.ravel()   # must be 1D for 
         else:
             return lsfmodel
         
@@ -79,7 +93,7 @@ def fitghpars(w,comblsf,initparams):
     nbd = len(bd)
     sigma = comblsf*0+1.0
     sigma[bd] = 1e10
-    
+
     # bounds=bounds
     # Data must be flattened to 1D for curve_fit
     pinit = fitter.coeffs.copy()
@@ -87,15 +101,73 @@ def fitghpars(w,comblsf,initparams):
     #w2 = np.tile(w,(nlsf,1)).T
     #pars, cov = curve_fit(fitter.model, w2.flatten(), comblsf.flatten(), sigma=sigma.flatten(), p0=pinit)
     x = np.arange(npix)
-    pars, cov = curve_fit(fitter.model, x, comblsf.flatten(), sigma=sigma.flatten(), p0=pinit)
+
+    # fit every 20th column to speed up    
+    xx = x[::20]
+    yy = comblsf[::20,:].ravel()
+    err = sigma[::20,:].ravel()
+    
+    pars, cov = curve_fit(fitter.model, xx, yy, sigma=err, p0=pinit, maxfev=50000)
+    pars2, cov2 = curve_fit(fitter.model, x, comblsf.ravel(), sigma=sigma.ravel(), p0=pinit, maxfev=50000)
     perr = np.sqrt(np.diag(cov))
 
     # Final LSF model and full parameter array
-    lsfmodel = fitter.model(x,*pars,flatten=False)
+    lsfmodel = fitter.model(x,*pars,ravel=False)
     lsfpars = fitter.loadcoefs(pars)
 
+    lsfmodel2 = fitter.model(x,*pars2,ravel=False)
+    lsfpars2 = fitter.loadcoefs(pars2)
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+    matplotlib.use('Qt5Agg')
+    plt.ion()
+
+    params=doplsf.unpack_ghlsf_params(fitter.loadcoefs(pars))
+    params2=doplsf.unpack_ghlsf_params(fitter.loadcoefs(pars2))
+
+    print('rms=',np.sqrt(np.nanmean((comblsf-lsfmodel)**2)))
+    print('rms=',np.sqrt(np.nanmean((comblsf-lsfmodel2)**2)))
+    
+    import pdb; pdb.set_trace()
+    
     return pars, perr, lsfpars, lsfmodel
 
+def getcoefficients(x,xcenter,params):
+    nowings = False
+    
+    # Parse x
+    if len(x.shape) == 1:
+        x = np.tile(x,(len(xcenter),1))
+    # Unpack the LSF parameters
+    if type(params) is not dict:
+        params = doplsf.unpack_ghlsf_params(params)
+    # Get the wing parameters at each x
+    wingparams = np.empty((params['nWpar'],len(xcenter)))
+    for ii in range(params['nWpar']):
+        poly = np.polynomial.Polynomial(params['Wcoefs'][ii])
+        wingparams[ii] = poly(xcenter+params['Xoffset'])
+    # Get the GH parameters at each x
+    ghparams = np.empty((params['Horder']+1,len(xcenter)))
+
+    # note that this is modified/corrected a bit from Bovy's routines based on comparison
+    # with LSF from IDL routines, noticeable when wings are non-negligible
+    for ii in range(params['Horder']+1):
+        poly = np.polynomial.Polynomial(params['GHcoefs'][ii])
+        ghparams[ii] = poly(xcenter+params['Xoffset'])
+   # for ii in range(params['Horder']+2):
+   #     if ii == 1:
+   #         ghparams[ii] = 1.
+   #     else:
+   #         poly = np.polynomial.Polynomial(params['GHcoefs'][ii-(ii > 1)])
+   #         ghparams[ii] = poly(xcenter+params['Xoffset'])
+   #     # normalization
+   #     if ii > 0:
+   #         ghparams[ii] /= np.sqrt(2.*np.pi*math.factorial(ii-1))
+   #         if not nowings: ghparams[ii] *= (1.-wingparams[0])
+            
+    return ghparams,wingparams
+    
 
 def lsfvisitcomb(visitfiles):
     """
@@ -125,8 +197,39 @@ def lsfvisitcomb(visitfiles):
             print('  '+str(o)+' '+str(len(g)))
             lsf1[g,:] += specarr[s].lsf.anyarray(wstar[g],xtype='wave',order=o,nlsf=15,original=False)
         lsfarr[:,:,s] = lsf1
-            
-    # Get S/N per star
+
+
+    # get the GH coefficients
+    lsfpars = specarr[0].lsf.pars[:,1].copy()
+    vwave = specarr[0].lsf.wave[:,1].copy()
+    xlsf = np.arange(nlsf)-nlsf//2
+    xcenter = np.arange(4096)
+    ghparams,wingparams = getcoefficients(xlsf,xcenter,lsfpars)
+    # (6, 4096)
+    
+    # Get x-values on the final scale
+    wstar = apStarWave()
+    xstar = np.arange(8575)
+    xint = np.interp(vwave,wstar,xstar)
+    # fit new GH coefficients on the new x-array
+    params = doplsf.unpack_ghlsf_params(lsfpars)
+    porder = params['Porder']
+    xoffset = -npix//2
+    coefarr = np.zeros((6,3),float)
+    for i in range(6):
+        coef = np.polyfit(xint+xoffset,ghparams[i,:],porder[i])
+        coefarr[i,:porder[i]+1] = coef
+
+    # Make new params
+    newparams = params.copy()
+    newparams['binsize'] = 1
+    newparams['Xoffset'] = xoffset
+    newparams['Porder'][0] = 2
+    newparams['GHcoefs'] = coefarr
+    newlsfpars = doplsf.repack_ghlsf_params(newparams)
+    
+    
+    # Get S/N per visit
     snarr = np.zeros(nspec,float)
     for i in range(nspec):
         snarr[i] = specarr[i].snr
@@ -149,11 +252,57 @@ def lsfvisitcomb(visitfiles):
     #params = doplsf.unpack_ghlsf_params(specarr[0].lsf.pars[:,1])
     #params['Xoffset'] = npix//2
     #coeffs = np.hstack((params['GHcoefs'].flatten(),params['Wcoefs'].flatten()))
-    lsfpars = specarr[0].lsf.pars[:,1]
-    lsfpars[1] = npix//2
+    #lsfpars = specarr[0].lsf.pars[:,1].copy()
+    #lsfpars[1] = npix//2
+
+    # an estimate from a dr12 apStar LSF
+    #lsfpars = np.array([ 1.00000000e+00,  4.28700000e+03,  5.00000000e+00,  2.00000000e+00,
+    #                     1.00000000e+00,  1.00000000e+00,  1.00000000e+00,  1.00000000e+00,
+    #                     0.00000000e+00,  2.21925064e+00, -1.22070496e-04,  6.05504495e-09,
+    #                     -2.10190968e-02,  1.39464958e-06, -1.70763672e-01, -2.25294252e-07,
+    #                     8.01283666e-03, -5.56369138e-07,  3.21678185e-01, -1.30227820e-05,
+    #                     -1.99480173e-02,  1.00000000e+00,  2.00000000e+00,  0.00000000e+00,
+    #                     0.00000000e+00,  9.99999978e-03,  2.84032046e-02])
+
+    # try with fewer GH components
+    # one
+    #lsfpars = np.array([ 1.00000000e+00,  4.28700000e+03,  0.00000000e+00,  2.00000000e+00,
+    #                     2.21925064e+00, -1.22070496e-04,  6.05504495e-09,
+    #                     1.00000000e+00,  2.00000000e+00,  0.00000000e+00,
+    #                     0.00000000e+00,  9.99999978e-03,  2.84032046e-02])
+    # two (order 2 0)
+    #lsfpars = np.array([ 1.00000000e+00,  4.28700000e+03,  1.00000000e+00,  2.00000000e+00,
+    #                     0.0,
+    #                     2.21925064e+00, -1.22070496e-04,  6.05504495e-09,
+    #                     -2.10190968e-02,
+    #                     1.00000000e+00,  2.00000000e+00,  0.00000000e+00,
+    #                     0.00000000e+00,  9.99999978e-03,  2.84032046e-02])
+
+    # two (order 2 1)
+    #lsfpars = np.array([ 1.00000000e+00,  4.28700000e+03,  1.00000000e+00,  2.00000000e+00,
+    #                     1.0,
+    #                     2.21925064e+00, -1.22070496e-04,  6.05504495e-09,
+    #                     -2.10190968e-02,  1.39464958e-06,
+    #                     1.00000000e+00,  2.00000000e+00,  0.00000000e+00,
+    #                     0.00000000e+00,  9.99999978e-03,  2.84032046e-02])
+
+    # three (order 2 1 1)
+    #lsfpars = np.array([ 1.00000000e+00,  4.28700000e+03,  2.00000000e+00,  2.00000000e+00,
+    #                     1.0, 1.0,
+    #                     2.21925064e+00, -1.22070496e-04,  6.05504495e-09,
+    #                     -2.10190968e-02,  1.39464958e-06,
+    #                     -1.70763672e-01, -2.25294252e-07,                         
+    #                     1.00000000e+00,  2.00000000e+00,  0.00000000e+00,
+    #                     0.00000000e+00,  9.99999978e-03,  2.84032046e-02])
+
+    
+    
+    # binsize=1
+    
+    #import pdb; pdb.set_trace()
     
     # Now fit LSF Gauss-Hermite parameters to the combined LSF
-    coefs,coeferr,finalpars,lsfmodel = fitghpars(wstar,comblsf,lsfpars)
+    coefs,coeferr,finalpars,lsfmodel = fitghpars(wstar,comblsf,newlsfpars)
     
     return comblsf,coefs,coeferr,finalpars,lsfmodel
 
